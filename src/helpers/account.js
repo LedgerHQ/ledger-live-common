ef33cf4930135f1e8494ad41ded3ccfc607ba0f2
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2018-10-10 16:44:02 +0200

    Add sortAccounts utility

--- src/helpers/account.js
+++ src/helpers/account.js
@@ -212,3 +212,44 @@ export const validateNameEdition = (account: Account, name: ?string): string =>
     account.name ||
     getAccountPlaceholderName(account)
   ).slice(0, MAX_ACCOUNT_NAME_SIZE);
+
+export type SortAccountsParam = {
+  accounts: Account[],
+  accountsBtcBalance: BigNumber[],
+  orderAccounts: string
+};
+
+type SortMethod = "name" | "balance";
+
+const sortMethod: { [_: SortMethod]: (SortAccountsParam) => string[] } = {
+  balance: ({ accounts, accountsBtcBalance }) =>
+    accounts
+      .map((a, i) => [a.id, accountsBtcBalance[i], a.name])
+      .sort((a, b) => {
+        const numOrder = a[1].minus(b[1]).toNumber();
+        if (numOrder === 0) {
+          return a[2].localeCompare(b[2]);
+        }
+
+        return numOrder;
+      })
+      .map(o => o[0]),
+
+  name: ({ accounts }) =>
+    accounts
+      .slice(0)
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map(a => a.id)
+};
+
+export function sortAccounts(param: SortAccountsParam) {
+  const [order, sort] = param.orderAccounts.split("|");
+  if (order === "name" || order === "balance") {
+    const ids = sortMethod[order](param);
+    if (sort === "desc") {
+      ids.reverse();
+    }
+    return ids;
+  }
+  return null;
+}
