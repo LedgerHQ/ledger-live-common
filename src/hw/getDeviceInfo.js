21ad3622dfe12fa371e8e7e68d8fc684ffabe20d
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-04-02 14:32:12 +0200

    Fix getDeviceInfo for rc1 versions

--- src/hw/getDeviceInfo.js
+++ src/hw/getDeviceInfo.js
@@ -10,7 +10,7 @@ const PROVIDERS: { [_: string]: number } = {
   "": 1,
   das: 2,
   club: 3,
-  shitcoins: 4,
+  test: 4,
   ee: 5
 };
 
@@ -22,17 +22,16 @@ export default async (transport: Transport<*>): Promise<DeviceInfo> => {
   const { seVersion } = res;
   const { targetId, mcuVersion, flags } = res;
   const parsedVersion =
-    seVersion.match(/([0-9]+.[0-9])+(.[0-9]+)?((?!-osu)-([a-z]+))?(-osu)?/) ||
-    [];
+    seVersion.match(
+      /([0-9]+.[0-9])+(.[0-9]+)?((?!-osu)-([a-zA-Z0-9]+))?(-osu)?/
+    ) || [];
   const isOSU = typeof parsedVersion[5] !== "undefined";
-  const providerName = parsedVersion[4] || "";
-  const providerId = getEnv("FORCE_PROVIDER") || PROVIDERS[providerName];
+  const tag = parsedVersion[4] || "";
+  const providerId = getEnv("FORCE_PROVIDER") || PROVIDERS[tag] || 1;
   const isBootloader = (targetId & 0xf0000000) !== 0x30000000;
   const majMin = parsedVersion[1];
   const patch = parsedVersion[2] || ".0";
-  const fullVersion = `${majMin}${patch}${
-    providerName ? `-${providerName}` : ""
-  }`;
+  const fullVersion = `${majMin}${patch}${tag ? `-${tag}` : ""}`;
   const flag = flags.length > 0 ? flags[0] : 0;
   const managerAllowed = !!(flag & ManagerAllowedFlag);
   const pinValidated = !!(flag & PinValidatedFlag);
@@ -43,7 +42,6 @@ export default async (transport: Transport<*>): Promise<DeviceInfo> => {
     isOSU,
     mcuVersion,
     isBootloader,
-    providerName,
     providerId,
     flags,
     managerAllowed,
