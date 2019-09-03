4e6f194901285bf069fa574e049a3d2fb4e45ccf
Juan Cortes Ross <juan@bohem.io>
2019-09-03 22:39:29 +0200

    Fixes the the ripple signAndBroadcast implementation, signing seems broken

--- src/families/ripple/libcore-signAndBroadcast.js
+++ src/families/ripple/libcore-signAndBroadcast.js
@@ -1,37 +1,48 @@
 // @flow
 
 import { BigNumber } from "bignumber.js";
-import type { Operation } from "../../types";
+import type { Account, Operation } from "../../types";
 import { getEnv } from "../../env";
+import { libcoreBigIntToBigNumber } from "../../libcore/buildBigNumber";
+import type { CoreRippleLikeTransaction, Transaction } from "../ethereum/types";
+import type { CoreAccount } from "../../libcore/types";
 
 async function ripple({
-  account: { id: accountId },
+  account: { id: accountId, freshAddress },
   signedTransaction,
   builded,
   coreAccount,
   transaction
-}: *) {
+}: {
+  account: Account,
+  signedTransaction: string,
+  builded: CoreRippleLikeTransaction,
+  coreAccount: CoreAccount,
+  transaction: Transaction
+}) {
   const rippleLikeAccount = await coreAccount.asRippleLikeAccount();
 
+  // FIXME, it doesn't pass this point, broadcastRawTransaction fails.
+  // Disabling the broadcast succeeds, hash is empty. Signing broken?
   const txHash = getEnv("DISABLE_TRANSACTION_BROADCAST")
     ? ""
     : await rippleLikeAccount.broadcastRawTransaction(signedTransaction);
   const senders = [freshAddress];
   const receiver = await builded.getReceiver();
   const recipients = [await receiver.toBase58()];
-  const fee = await transaction.getFees();
-  const tag = await transaction.getDestinationTag();
-  const transactionSequenceNumberRaw = await transaction.getSequence();
-  const transactionSequenceNumber = await libcoreBigIntToBigNumber(
+  const fee = await builded.getFees();
+  const tag = await builded.getDestinationTag();
+  const transactionSequenceNumberRaw = await builded.getSequence();
+  const transactionSequenceNumber = (await libcoreBigIntToBigNumber(
     transactionSequenceNumberRaw
-  ).toNumber();
+  )).toNumber();
 
   const op: $Exact<Operation> = {
     id: `${accountId}-${txHash}-OUT`,
     hash: txHash,
     type: "OUT",
     value: transaction.useAllAmount
-      ? balance
+      ? account.balance
       : BigNumber(transaction.amount || 0).plus(fee),
     fee,
     blockHash: null,
