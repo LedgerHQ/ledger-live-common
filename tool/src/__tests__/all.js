de68c14fcfd55c5b32a1b5345c1e4cabf664b56f
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-09-04 09:11:42 +0200

    refine tests

--- tool/src/__tests__/all.js
+++ tool/src/__tests__/all.js
@@ -3,6 +3,7 @@
 
 import "babel-polyfill";
 import axios from "axios";
+import { BigNumber } from "bignumber.js";
 import type { Account, AccountBridge } from "@ledgerhq/live-common/lib/types";
 import { setEnv } from "@ledgerhq/live-common/lib/env";
 import { fromAccountRaw } from "@ledgerhq/live-common/lib/account";
@@ -132,44 +133,62 @@ const doge1: Account = fromAccountRaw({
 });
 */
 
-describe("properties shared accross all bridges", () => {
-  const bridgeImplementations = {
-    bitcoin: ["mock", ""],
-    ethereum: ["mock", "js", "libcore"],
-    xrp: ["mock", "js", "libcore"]
-  };
+const bridgeImplementations = {
+  bitcoin: ["mock", ""],
+  ethereum: ["mock", "js", "libcore"],
+  xrp: ["mock", "js", "libcore"]
+};
 
-  // utility to test common behaviors shared on all bridges
-  const forEachDifferentBridge = async (
-    f: (
-      Account,
-      AccountBridge<any>,
-      { family: string, impl: string }
-    ) => Promise<any>
-  ) => {
-    for (let account of [ethereum1, xrp1, bitcoin1]) {
-      const { family } = account.currency;
-      const impls = bridgeImplementations[family] || [""];
-      for (let impl of impls) {
-        setEnv("BRIDGE_FORCE_IMPLEMENTATION", impl);
-        const bridge = getAccountBridge(account, null);
-        console.log(family, impl);
-        await f(account, bridge, { family, impl });
-      }
-    }
-  };
+mapDifferentBridge((account, bridge, meta) => [
+  bridge
+    .startSync(account, false)
+    .toPromise()
+    .then(f => f(account)),
+  bridge,
+  meta
+]).forEach(([accountPromise, bridge, { family, impl }]) => {
+  describe("shared properties on: " + family + " " + impl, () => {
+    describe("prepareTransaction", () => {
+      // stability: function called twice will return the same object reference (=== convergence so we can stop looping, typically because transaction will be a hook effect dependency of prepareTransaction)
 
-  beforeEach(() => {
-    setEnv("BRIDGE_FORCE_IMPLEMENTATION", "");
-  });
+      test("stability on empty transaction", async () => {
+        const account = await accountPromise;
+        const t = bridge.createTransaction(account);
+        const t2 = await bridge.prepareTransaction(account, t);
+        const t3 = await bridge.prepareTransaction(account, t2);
+        expect(t2).toBe(t3);
+      });
 
-  test("prepareTransaction called twice will return the same object reference", async () => {
-    await forEachDifferentBridge(async (account, bridge) => {
-      await bridge.startSync(account, false).toPromise();
-      const t = bridge.createTransaction(account);
-      const t2 = await bridge.prepareTransaction(account, t);
-      const t3 = await bridge.prepareTransaction(account, t2);
-      expect(t2).toBe(t3);
+      // stability: function called twice will return the same object reference (=== convergence so we can stop looping, typically because transaction will be a hook effect dependency of prepareTransaction)
+
+      test("stability on self transaction", async () => {
+        const account = await accountPromise;
+        const t = {
+          ...bridge.createTransaction(account),
+          amount: BigNumber(1000),
+          recipient: account.freshAddress
+        };
+        const t2 = await bridge.prepareTransaction(account, t);
+        const t3 = await bridge.prepareTransaction(account, t2);
+        expect(t2).toBe(t3);
+      });
     });
   });
 });
+
+// utility to test common behaviors shared on all bridges
+function mapDifferentBridge(
+  f: (Account, AccountBridge<any>, { family: string, impl: string }) => any
+) {
+  const all = [];
+  for (let account of [ethereum1, xrp1, bitcoin1]) {
+    const { family } = account.currency;
+    const impls = bridgeImplementations[family] || [""];
+    for (let impl of impls) {
+      setEnv("BRIDGE_FORCE_IMPLEMENTATION", impl);
+      const bridge = getAccountBridge(account, null);
+      all.push(f(account, bridge, { family, impl }));
+    }
+  }
+  return all;
+}
