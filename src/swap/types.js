852836f507d725a87ed3c7f188679f7415ffe751
Juan Cortes Ross <juan@bohem.io>
2020-02-27 12:04:15 +0100

    Updated swap types

--- src/swap/types.js
+++ src/swap/types.js
@@ -1,38 +1,33 @@
 // @flow
 
-import BigNumber from "bignumber.js";
-import { AccountLike } from "../types/account";
+import { BigNumber } from "bignumber.js";
+import type { Account, AccountLike } from "../types/account";
+import type { DeviceId, Transaction } from "../types";
 
 // in bridge? heuristic. error could happen later.
 export type EstimateMaxSpendable = (account: Account) => BigNumber;
 
 // description of the initial form
 export type Exchange = {
-  // FIXME what types to use exactly? check getMainAccount()
   fromParentAccount: ?Account,
   fromAccount: AccountLike,
-
   toParentAccount: ?Account,
   toAccount: AccountLike,
-
   fromAmount: BigNumber,
   sendMax: boolean
 };
 
 export type ExchangeRate = {
-  // FIXME is this rate fixed?
   rate: BigNumber,
   rateId: string,
   provider: string,
-  providerURL: string, // FIXME not available?
-  expirationDate: ?Date // FIXME not available?
+  providerURL: ?string,
+  expirationDate: ?Date // FIXME not available? Asked channel where we get this from
 };
 
 // to be called every time Exchange changes
 // it could fail if the exchange is not possible (out of range)
 export type GetExchangeRate = Exchange => Promise<ExchangeRate>;
-
-// FIXME: does the swap provider expect us to use the EXACT amount? can we have some approx?
 
 // init a swap with the Exchange app
 // throw if TransactionStatus have errors
@@ -45,6 +40,7 @@ export type InitSwap = ({
   transaction: Transaction,
   swapId: string
 }>;
+
 /*
 // TO BE FIGURED OUT
 type FamilySwapSpecifics = {
@@ -56,16 +52,25 @@ type FamilySwapSpecifics = {
 // bridge.signOperation
 // bridge.broadcast => Operation
 
-/*
 // META: this is what the UI "operation" will need
-type SwapOperation = {
-  exchange: Exchange,
-  exchangeStatus: ExchangeStatus,
-  operation: Operation,
-  swapId: string
-};
-*/
+// type SwapOperation = {
+//   exchange: Exchange,
+//   exchangeStatus: ExchangeStatus, // TODO what is ExchangeStatus
+//   operation: Operation,
+//   swapId: string
+// };
 
-type SwapStatus = string;
+type SwapStatus =
+  | "Confirming"
+  | "Finished"
+  | "Exchanging"
+  | "Hold"
+  | "Sending"
+  | "Waiting"
+  | "Overdue"
+  | "Refunded"
+  | "New"
+  | "Expired"
+  | "Failed";
 
 export type GetSwapStatus = ({ swapId: string }) => Promise<SwapStatus>;
