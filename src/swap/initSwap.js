305a872d36c8a3a2511b0dfa695c043d970a5ea5
Juan Cortes Ross <juan@bohem.io>
2020-03-04 22:37:40 +0100

    Added transaction status check inside initSwap

--- src/swap/initSwap.js
+++ src/swap/initSwap.js
@@ -1,24 +1,24 @@
 // @flow
-/* eslint-disable no-console */
 
 import secp256k1 from "secp256k1";
 import Swap from "@ledgerhq/hw-app-swap";
-import perFamily from "../generated/swap";
 import { from } from "rxjs";
+import perFamily from "../generated/swap";
+import type { CryptoCurrency } from "../types/currencies";
+import type { Transaction } from "../generated/types";
+import { getAccountCurrency, getMainAccount } from "../account";
+import network from "../network";
+import { getAccountBridge } from "../bridge";
 import type {
   Exchange,
   ExchangeRate,
   InitSwap,
   SwapProviderNameAndSignature
 } from "./types";
-import type { Account } from "../types";
-import type { CryptoCurrency } from "../types/currencies";
-import type { Transaction } from "../generated/types";
-import { getAccountCurrency, getMainAccount } from "../account";
-import network from "../network";
-import { getAccountBridge } from "../bridge";
-import { getCurrencySwapConfig, getSwapProviders, swapAPIBaseURL } from "./";
 import { withDevice } from "../hw/deviceAccess";
+import type { Account } from "../types";
+import { getCurrencySwapConfig, getSwapProviders, swapAPIBaseURL } from "./";
+import { BigNumber } from "bignumber.js";
 
 const initSwap: InitSwap = async (
   exchange: Exchange,
@@ -69,7 +69,7 @@ const initSwap: InitSwap = async (
     const { swapId, provider: providerName } = swapResult;
     const provider = getSwapProviders(providerName);
 
-    // FIXME because this would only work for currencies right now
+    // FIXME because this would break for tokens
     if (payoutCurrency.type !== "CryptoCurrency") {
       throw new Error("How do I handle non CryptoCurrencies");
     }
@@ -92,22 +92,29 @@ const initSwap: InitSwap = async (
     ).toPromise();
 
     const accountBridge = getAccountBridge(refundAccount);
-    console.log("⁍ Creating transaction");
     let transaction = accountBridge.createTransaction(refundAccount);
 
-    console.log("⁍ Updating transaction");
+    // FIXME we send decimals but swap wants satoshis
     transaction = accountBridge.updateTransaction(transaction, {
-      amount: swapResult.amountExpectedFrom,
+      amount: BigNumber(
+        swapResult.amountExpectedFrom * 10 ** refundCurrency.units[0].magnitude
+      ),
       recipient: swapResult.payinAddress
     });
-    console.log("⁍ Preparing transaction");
+
     transaction = await accountBridge.prepareTransaction(
       refundAccount,
       transaction
     );
 
-    // FIXME we send decimals but swap wants satoshis
-    transaction.amount = transaction.amount * 10 ** 8;
+    const { errors } = await accountBridge.getTransactionStatus(
+      refundAccount,
+      transaction
+    );
+
+    if (errors.recipient || errors.amount) {
+      throw errors.recipient || errors.amount;
+    }
 
     return { transaction, swapId };
   }
@@ -133,27 +140,18 @@ const performSwapChecks = async ({
   swapResult: *
 }) => {
   const swap = new Swap(transport);
-  console.log("⁍ Setting partner on swap");
   await swap.setPartnerKey(provider.nameAndPubkey);
-  console.log("⁍ Checking partner on swap");
   await swap.checkPartner(provider.signature);
-  console.log("⁍ Process the transaction payload");
   await swap.processTransaction(Buffer.from(swapResult.binaryPayload, "hex"));
   const goodSign = secp256k1.signatureExport(
     Buffer.from(swapResult.signature, "hex")
   );
-  console.log("⁍ Checking transaction signature");
   await swap.checkTransactionSignature(goodSign);
-
-  console.log(
-    `⁍ Fetching address parameters from ${payoutAccount.freshAddressPath}`
-  );
-
   const payoutAddressParameters = await perFamily[
     payoutCurrency.family
   ].getSerializedAddressParameters(payoutAccount.freshAddressPath, "p2sh");
+  // FIXME need to fetch this `format` from the account somehow     ↑
 
-  console.log("⁍ Checking payout address config/signature");
   const {
     config: payoutAddressConfig,
     signature: payoutAddressConfigSignature
@@ -168,8 +166,8 @@ const performSwapChecks = async ({
   const refundAddressParameters = await perFamily[
     refundCurrency.family
   ].getSerializedAddressParameters(refundAccount.freshAddressPath, "bech32");
+  // FIXME need to fetch this `format` from the account somehow     ↑
 
-  console.log("⁍ Checking refund address config/signature");
   const {
     config: refundAddressConfig,
     signature: refundAddressConfigSignature
