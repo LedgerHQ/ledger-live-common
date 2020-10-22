78d6ce8a9aed5e61898751c0b7d9d9b83485f6ff
Hugo Morosini <hugo.morosini@gmail.com>
2020-10-22 11:59:44 +0200

    transaction signing OK

--- src/sell/getTransactionId.js
+++ src/sell/getTransactionId.js
@@ -2,7 +2,12 @@
 import type Transport from "@ledgerhq/hw-transport";
 import Sell from "./hw-app-sell/Sell";
 
+function base64EncodeUrl(str) {
+  return str.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+}
+
 export default async (transport: Transport<*>): Promise<string> => {
-  const sell = new Sell(transport);
-  return sell.startNewTransaction();
+  const sell = new Sell(transport, 0x01);
+  const txId = await sell.startNewTransaction();
+  return base64EncodeUrl(txId.toString("base64"));
 };
