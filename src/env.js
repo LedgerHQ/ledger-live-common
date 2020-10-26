0727668762639bcb1cf9a2095db859dc3ebb306a
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-10-26 18:32:28 +0100

    using staging

--- src/env.js
+++ src/env.js
@@ -201,7 +201,7 @@ const envDefinitions = {
     desc: "Ledger generic explorer API",
   },
   EXPLORER_BETA: {
-    def: "https://explorers.api.live.ledger.com",
+    def: "https://explorers.api-01.live.ledger-stg.com",
     parser: stringParser,
     desc: "Ledger generic explorer beta API",
   },
