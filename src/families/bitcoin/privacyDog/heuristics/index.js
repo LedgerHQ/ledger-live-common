dc323fc20a7ccab50dc502f647ef57040955cd9b
Anirudha Bose <anirudha.bose@ledger.fr>
2020-12-17 05:55:26 +0100

    Implement script-types heuristics to determine recipient

--- src/families/bitcoin/privacyDog/heuristics/index.js
+++ src/families/bitcoin/privacyDog/heuristics/index.js
@@ -7,6 +7,7 @@ import { sendAddressReuse } from "./sendAddressReuse";
 import { outputValue1000x } from "./outputValue1000x";
 import { simpleCoinjoin } from "./simpleCoinjoin";
 import { samouraiWhirlpool } from "./samouraiWhirlpool";
+import { scriptTypes } from "./scriptTypes";
 
 export const heuristics: Heuristic[] = [
   {
@@ -37,6 +38,11 @@ export const heuristics: Heuristic[] = [
   {
     id: "samourai-whirlpool",
     handler: samouraiWhirlpool,
+    penaltyFactor: 0,
+  },
+  {
+    id: "script-types",
+    handler: scriptTypes,
     penaltyFactor: 0,
   },
 ];
