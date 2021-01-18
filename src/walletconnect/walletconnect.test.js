a15095ef7075dbe8b7a4259258af797121883c7f
machard <matthieu__@hotmail.fr>
2021-01-18 14:06:51 +0100

    fix lint

--- src/walletconnect/walletconnect.test.js
+++ src/walletconnect/walletconnect.test.js
@@ -189,7 +189,7 @@ describe("walletconnect", () => {
           {
             name: "_spender",
             value: "0x7a250d5630b4cf539739df2c5dacb4c659f2488d",
-            type: "address"
+            type: "address",
           },
           {
             name: "_value",
