cdf86f847444843c88fc9803063c74788ef65067
Henri Ly <henri@softlydev.com>
2021-04-01 15:23:23 +0200

    [LL-4977] fee strat btc + add feestrat to transaction type

--- src/families/ethereum/react.js
+++ src/families/ethereum/react.js
@@ -1,20 +1,31 @@
 // @flow
-import { useState, useEffect, useMemo, useRef } from "react";
-
+import invariant from "invariant";
+import { getGasLimit } from "./transaction";
 import type { Transaction } from "./types";
+import type { FeeStrategy } from "../../types";
 
-export function useFeesStrategy(t: Transaction) {
-  const [feeStrategy, setFeesStrategy] = useState([]);
+export function useFeesStrategy(t: Transaction): FeeStrategy[] {
+  const networkInfo = t.networkInfo;
+  invariant(networkInfo, "no network info");
+  const gasLimit = getGasLimit(t);
 
-  useEffect(() => {
-    const strategies = [
-      { name: "low", amount: t.networkInfo?.gasPrice.min },
-      { name: "medium", amount: t.networkInfo?.gasPrice.initial },
-      { name: "high", amount: t.networkInfo?.gasPrice.max },
-    ];
+  const strategies = [
+    {
+      label: "low",
+      amount: networkInfo.gasPrice.min,
+      displayedAmount: networkInfo.gasPrice.min.multipliedBy(gasLimit),
+    },
+    {
+      label: "medium",
+      amount: networkInfo.gasPrice.initial,
+      displayedAmount: networkInfo.gasPrice.initial.multipliedBy(gasLimit),
+    },
+    {
+      label: "high",
+      amount: networkInfo.gasPrice.max,
+      displayedAmount: networkInfo.gasPrice.max.multipliedBy(gasLimit),
+    },
+  ];
 
-    setFeesStrategy(strategies);
-  }, [t]);
-
-  return feeStrategy;
+  return strategies;
 }
