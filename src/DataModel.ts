41bf2c0552a99ae1d4ecffdbfc1cce76f3593aed
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-04 16:18:47 +0200

    DataModel.js

--- src/DataModel.ts
+++ src/DataModel.ts
@@ -1,20 +1,20 @@
 /**
- * @flow
- * @module DataModel
- */
-
-/**
  * Interface for the end user.
  * @memberof DataModel
  */
 export type DataModel<R, M> = {
   // R: Raw , M: Model
   // import a given version of rawData back into model
-  decode(rawModel: { data: R, version: number }): M,
+  decode(rawModel: { data: R; version: number }): M;
   // export data into a serializable object (can be saved to a JSON file)
-  encode(model: M): { data: R, version: number },
+  encode(
+    model: M
+  ): {
+    data: R;
+    version: number;
+  };
   // current version of the model
-  version: number,
+  version: number;
 };
 
 /**
@@ -23,11 +23,11 @@ export type DataModel<R, M> = {
  */
 export type DataSchema<R, M> = {
   // write extra logic to transform raw data into your model
-  decode(raw: R): M,
+  decode(raw: R): M;
   // reverse version of wrap, that will transform it back to a serializable object
-  encode(data: M): R,
+  encode(data: M): R;
   // A map of migrations functions that are unrolled when an old version is imported
-  migrations: Array<(any) => R | any>,
+  migrations: Array<(arg0: any) => R | any>;
 };
 
 /**
@@ -38,17 +38,29 @@ export function createDataModel<R, M>(
 ): DataModel<R, M> {
   const { migrations, encode, decode } = schema;
   const version = migrations.length;
+
   function decodeModel(raw) {
     let { data } = raw;
+
     for (let i = raw.version; i < version; i++) {
       data = migrations[i](data);
     }
+
     data = decode(data);
     return data;
   }
+
   function encodeModel(model) {
     const data = encode(model);
-    return { data, version };
+    return {
+      data,
+      version,
+    };
   }
-  return Object.freeze({ version, decode: decodeModel, encode: encodeModel });
+
+  return Object.freeze({
+    version,
+    decode: decodeModel,
+    encode: encodeModel,
+  });
 }
