1376501dabfde002b8c9d3c90a0e16fceaa9bcaf
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-04 14:45:19 +0200

    cache.js

--- src/cache.ts
+++ src/cache.ts
@@ -1,25 +1,22 @@
-// @flow
-
 import LRU from "lru-cache";
-
-export type CacheRes<A, T> = {
-  (...args: A): Promise<T>,
-  force: (...args: A) => Promise<T>,
-  hydrate: (string, T) => void,
-  clear: (string) => void,
-  reset: () => void,
+export type CacheRes<A extends Array<any>, T> = {
+  (...args: A): Promise<T>;
+  force: (...args: A) => Promise<T>;
+  hydrate: (arg0: string, arg1: T) => void;
+  clear: (arg0: string) => void;
+  reset: () => void;
 };
-
-export const makeLRUCache = <A: Array<*>, T>(
+export const makeLRUCache = <A extends Array<any>, T>(
   f: (...args: A) => Promise<T>,
   keyExtractor: (...args: A) => string = () => "",
-  lruOpts: Object = {
+  lruOpts: Record<string, any> = {
     max: 100,
     maxAge: 5 * 60 * 1000,
   }
 ): CacheRes<A, T> => {
   const cache = new LRU(lruOpts);
-  const result = (...args) => {
+
+  const result = (...args: A) => {
     const key = keyExtractor(...args);
     let promise = cache.get(key);
     if (promise) return promise;
@@ -30,23 +27,28 @@ export const makeLRUCache = <A: Array<*>, T>(
     cache.set(key, promise);
     return promise;
   };
-  result.force = (...args) => {
+
+  result.force = (...args: A) => {
     const key = keyExtractor(...args);
-    let promise = f(...args).catch((e) => {
+    const promise = f(...args).catch((e) => {
       cache.del(key);
       throw e;
     });
     cache.set(key, promise);
     return promise;
   };
+
   result.hydrate = (key: string, value: T) => {
     cache.set(key, Promise.resolve(value));
   };
+
   result.clear = (key: string) => {
     cache.del(key);
   };
+
   result.reset = () => {
     cache.reset();
   };
+
   return result;
 };
