a250d6b0f465cd281f4001c06057de1720baf2f7
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-04 14:54:03 +0200

    JSONBigNumber.js

--- src/JSONBigNumber.ts
+++ src/JSONBigNumber.ts
@@ -5,13 +5,10 @@
 //  NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 //  See http://www.JSON.org/js.html
 //  This code should be minified before deployment.
-
 //  USE YOUR OWN COPY. IT IS EXTREMELY UNWISE TO LOAD CODE FROM SERVERS YOU DO
 //  NOT CONTROL.
-
 //  This file creates a global JSONBigNumber object containing two methods: stringify
 //  and parse.
-
 //      JSONBigNumber.stringify(value, replacer, space)
 //          value       any JavaScript value, usually an object or array.
 //          replacer    an optional parameter that determines how object
@@ -31,9 +28,7 @@
 //          or undefined if nothing should be serialized. The toJSON method
 //          will be passed the key associated with the value, and this will be
 //          bound to the value.
-
 //          For example, this would serialize Dates as ISO strings.
-
 //              Date.prototype.toJSON = function (key) {
 //                  function f(n) {
 //                      // Format integers to have at least two digits.
@@ -48,63 +43,48 @@
 //                       f(this.getUTCMinutes())   + ":" +
 //                       f(this.getUTCSeconds())   + "Z";
 //              };
-
 //          You can provide an optional replacer method. It will be passed the
 //          key and value of each member, with this bound to the containing
 //          object. The value that is returned from your method will be
 //          serialized. If your method returns undefined, then the member will
 //          be excluded from the serialization.
-
 //          If the replacer parameter is an array of strings, then it will be
 //          used to select the members to be serialized. It filters the results
 //          such that only members with keys listed in the replacer array are
 //          stringified.
-
 //          Values that do not have JSON representations, such as undefined or
 //          functions, will not be serialized. Such values in objects will be
 //          dropped; in arrays they will be replaced with null. You can use
 //          a replacer function to replace those with JSON values.
-
 //          JSONBigNumber.stringify(undefined) returns undefined.
-
 //          The optional space parameter produces a stringification of the
 //          value that is filled with line breaks and indentation to make it
 //          easier to read.
-
 //          If the space parameter is a non-empty string, then that string will
 //          be used for indentation. If the space parameter is a number, then
 //          the indentation will be that many spaces.
-
 //          Example:
-
 //          text = JSONBigNumber.stringify(["e", {pluribus: "unum"}]);
 //          // text is '["e",{"pluribus":"unum"}]'
-
 //          text = JSONBigNumber.stringify(["e", {pluribus: "unum"}], null, "\t");
 //          // text is '[\n\t"e",\n\t{\n\t\t"pluribus": "unum"\n\t}\n]'
-
 //          text = JSONBigNumber.stringify([new Date()], function (key, value) {
 //              return this[key] instanceof Date
 //                  ? "Date(" + this[key] + ")"
 //                  : value;
 //          });
 //          // text is '["Date(---current time---)"]'
-
 //      JSONBigNumber.parse(text, reviver)
 //          This method parses a JSON text to produce an object or array.
 //          It can throw a SyntaxError exception.
-
 //          The optional reviver parameter is a function that can filter and
 //          transform the results. It receives each of the keys and values,
 //          and its return value is used instead of the original value.
 //          If it returns what it received, then the structure is not modified.
 //          If it returns undefined then the member is deleted.
-
 //          Example:
-
 //          // Parse the text. Values that look like ISO date strings will
 //          // be converted to Date objects.
-
 //          myData = JSONBigNumber.parse(text, function (key, value) {
 //              var a;
 //              if (typeof value === "string") {
@@ -117,7 +97,6 @@
 //              }
 //              return value;
 //          });
-
 //          myData = JSONBigNumber.parse('["Date(09/09/2001)"]', function (key, value) {
 //              var d;
 //              if (typeof value === "string" &&
@@ -130,12 +109,9 @@
 //              }
 //              return value;
 //          });
-
 //  This is a reference implementation. You are free to copy, modify, or
 //  redistribute.
-
 import BigNumber from "bignumber.js";
-
 var rx_escapable = /[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
 
 function f(n) {
@@ -148,6 +124,7 @@ function this_value() {
 }
 
 if (typeof Date.prototype.toJSON !== "function") {
+  // @ts-expect-error we need to redeclare the contructor type to extend the prototype
   Date.prototype.toJSON = function () {
     return isFinite(this.valueOf())
       ? this.getUTCFullYear() +
@@ -165,8 +142,11 @@ if (typeof Date.prototype.toJSON !== "function") {
       : null;
   };
 
+  // @ts-expect-error we need to redeclare the contructor type to extend the prototype
   Boolean.prototype.toJSON = this_value;
+  // @ts-expect-error we need to redeclare the contructor type to extend the prototype
   Number.prototype.toJSON = this_value;
+  // @ts-expect-error we need to redeclare the contructor type to extend the prototype
   String.prototype.toJSON = this_value;
 }
 
@@ -180,7 +160,6 @@ function quote(string) {
   // backslash characters, then we can safely slap some quotes around it.
   // Otherwise we must also replace the offending characters with safe escape
   // sequences.
-
   rx_escapable.lastIndex = 0;
   return rx_escapable.test(string)
     ? '"' +
@@ -196,10 +175,12 @@ function quote(string) {
 
 function str(key, holder) {
   // Produce a string from holder[key].
-
   var i; // The loop counter.
+
   var k; // The member key.
+
   var v; // The member value.
+
   var length;
   var mind = gap;
   var partial;
@@ -209,13 +190,11 @@ function str(key, holder) {
     (value instanceof BigNumber || BigNumber.isBigNumber(value));
 
   // Check for NaN and Infinity
-
   if (isBigNumber && !value.isFinite()) {
     value = null;
   }
 
   // If the value has a toJSON method, call it to obtain a replacement value.
-
   if (
     value &&
     typeof value === "object" &&
@@ -226,13 +205,11 @@ function str(key, holder) {
 
   // If we were called with a replacer function, then call the replacer to
   // obtain a replacement value.
-
   if (typeof rep === "function") {
     value = rep.call(holder, key, value);
   }
 
   // What happens next depends on the value's type.
-
   switch (typeof value) {
     case "string":
       if (isBigNumber) {
@@ -243,47 +220,39 @@ function str(key, holder) {
 
     case "number":
       // JSON numbers must be finite. Encode non-finite numbers as null.
-
       return isFinite(value) ? String(value) : "null";
 
     case "boolean":
-    case "null":
       // If the value is a boolean or null, convert it to a string. Note:
       // typeof null does not produce "null". The case is included here in
       // the remote chance that this gets fixed someday.
-
       return String(value);
 
     // If the type is "object", we might be dealing with an object or an array or
     // null.
-
     case "object":
       // Due to a specification blunder in ECMAScript, typeof null is "object",
       // so watch out for that case.
-
       if (!value) {
         return "null";
       }
 
       // Make an array to hold the partial results of stringifying this object value.
-
       gap += indent;
       partial = [];
 
       // Is the value an array?
-
       if (Object.prototype.toString.apply(value) === "[object Array]") {
         // The value is an array. Stringify every element. Use null as a placeholder
         // for non-JSON values.
-
         length = value.length;
+
         for (i = 0; i < length; i += 1) {
           partial[i] = str(i, value) || "null";
         }
 
         // Join all of the elements together, separated with commas, and wrap them in
         // brackets.
-
         v =
           partial.length === 0
             ? "[]"
@@ -295,13 +264,14 @@ function str(key, holder) {
       }
 
       // If the replacer is an array, use it to select the members to be stringified.
-
       if (rep && typeof rep === "object") {
         length = rep.length;
+
         for (i = 0; i < length; i += 1) {
           if (typeof rep[i] === "string") {
             k = rep[i];
             v = str(k, value);
+
             if (v) {
               partial.push(quote(k) + (gap ? ": " : ":") + v);
             }
@@ -309,10 +279,10 @@ function str(key, holder) {
         }
       } else {
         // Otherwise, iterate through all of the keys in the object.
-
         for (k in value) {
           if (Object.prototype.hasOwnProperty.call(value, k)) {
             v = str(k, value);
+
             if (v) {
               partial.push(quote(k) + (gap ? ": " : ":") + v);
             }
@@ -322,7 +292,6 @@ function str(key, holder) {
 
       // Join all of the member texts together, separated with commas,
       // and wrap them in braces.
-
       v =
         partial.length === 0
           ? "{}"
@@ -335,7 +304,6 @@ function str(key, holder) {
 }
 
 // If the JSON object does not yet have a stringify method, give it one.
-
 meta = {
   // table of character substitutions
   "\b": "\\b",
@@ -346,35 +314,30 @@ meta = {
   '"': '\\"',
   "\\": "\\\\",
 };
-
 export function stringify(value, replacer, space) {
   // The stringify method takes a value and an optional replacer, and an optional
   // space parameter, and returns a JSON text. The replacer can be a function
   // that can replace values, or an array of strings that will select the keys.
   // A default replacer method can be provided. Use of the space parameter can
   // produce text that is more easily readable.
-
   var i;
   gap = "";
   indent = "";
 
   // If the space parameter is a number, make an indent string containing that
   // many spaces.
-
   if (typeof space === "number") {
     for (i = 0; i < space; i += 1) {
       indent += " ";
-    }
-
-    // If the space parameter is a string, it will be used as the indent string.
+    } // If the space parameter is a string, it will be used as the indent string.
   } else if (typeof space === "string") {
     indent = space;
   }
 
   // If there is a replacer, it must be a function or an array.
   // Otherwise, throw an error.
-
   rep = replacer;
+
   if (
     replacer &&
     typeof replacer !== "function" &&
@@ -385,17 +348,16 @@ export function stringify(value, replacer, space) {
 
   // Make a fake root object containing our value under the key of "".
   // Return the result of stringifying the value.
-
   return str("", {
     "": value,
   });
 }
-
 // We are defining the function inside of another function to avoid creating
 // global variables.
-
 var at; // The index of the current character
+
 var ch; // The current character
+
 var escapee = {
   '"': '"',
   "\\": "\\",
@@ -410,7 +372,6 @@ var text;
 
 var error = function (m) {
   // Call error when something is wrong.
-
   throw {
     name: "SyntaxError",
     message: m,
@@ -419,16 +380,14 @@ var error = function (m) {
   };
 };
 
-var next = function (c) {
+var next = function (c?: any) {
   // If a c parameter is provided, verify that it matches the current character.
-
   if (c && c !== ch) {
     error("Expected '" + c + "' instead of '" + ch + "'");
   }
 
   // Get the next character. When there are no more characters,
   // return the empty string.
-
   ch = text.charAt(at);
   at += 1;
   return ch;
@@ -436,7 +395,6 @@ var next = function (c) {
 
 var number = function () {
   // Parse a number value to a BigNumber.
-
   var number;
   var string = "";
 
@@ -444,29 +402,37 @@ var number = function () {
     string = "-";
     next("-");
   }
+
   while (ch >= "0" && ch <= "9") {
     string += ch;
     next();
   }
+
   if (ch === ".") {
     string += ".";
+
     while (next() && ch >= "0" && ch <= "9") {
       string += ch;
     }
   }
+
   if (ch === "e" || ch === "E") {
     string += ch;
     next();
+
     if (ch === "-" || ch === "+") {
       string += ch;
       next();
     }
+
     while (ch >= "0" && ch <= "9") {
       string += ch;
       next();
     }
   }
+
   number = new BigNumber(string);
+
   if (!number.isFinite()) {
     error("Bad number");
   } else {
@@ -476,31 +442,35 @@ var number = function () {
 
 var string = function () {
   // Parse a string value.
-
   var hex;
   var i;
   var value = "";
   var uffff;
 
   // When parsing for string values, we must look for " and \ characters.
-
   if (ch === '"') {
     while (next()) {
       if (ch === '"') {
         next();
         return value;
       }
+
       if (ch === "\\") {
         next();
+
         if (ch === "u") {
           uffff = 0;
+
           for (i = 0; i < 4; i += 1) {
             hex = parseInt(next(), 16);
+
             if (!isFinite(hex)) {
               break;
             }
+
             uffff = uffff * 16 + hex;
           }
+
           value += String.fromCharCode(uffff);
         } else if (typeof escapee[ch] === "string") {
           value += escapee[ch];
@@ -512,12 +482,12 @@ var string = function () {
       }
     }
   }
+
   error("Bad string");
 };
 
 var white = function () {
   // Skip whitespace.
-
   while (ch && ch <= " ") {
     next();
   }
@@ -525,7 +495,6 @@ var white = function () {
 
 var word = function () {
   // true, false, or null.
-
   switch (ch) {
     case "t":
       next("t");
@@ -533,6 +502,7 @@ var word = function () {
       next("u");
       next("e");
       return true;
+
     case "f":
       next("f");
       next("a");
@@ -540,6 +510,7 @@ var word = function () {
       next("s");
       next("e");
       return false;
+
     case "n":
       next("n");
       next("u");
@@ -547,6 +518,7 @@ var word = function () {
       next("l");
       return null;
   }
+
   error("Unexpected '" + ch + "'");
 };
 
@@ -554,77 +526,91 @@ var value; // Place holder for the value function.
 
 var array = function () {
   // Parse an array value.
-
-  var arr = [];
+  var arr: any[] = [];
 
   if (ch === "[") {
     next("[");
     white();
+
     if (ch === "]") {
       next("]");
       return arr; // empty array
     }
+
     while (ch) {
       arr.push(value());
       white();
+
       if (ch === "]") {
         next("]");
         return arr;
       }
+
       next(",");
       white();
     }
   }
+
   error("Bad array");
 };
 
 var object = function () {
   // Parse an object value.
-
   var key;
   var obj = {};
 
   if (ch === "{") {
     next("{");
     white();
+
     if (ch === "}") {
       next("}");
       return obj; // empty object
     }
+
     while (ch) {
       key = string();
       white();
       next(":");
+
       if (Object.hasOwnProperty.call(obj, key)) {
         error("Duplicate key '" + key + "'");
       }
+
       obj[key] = value();
       white();
+
       if (ch === "}") {
         next("}");
         return obj;
       }
+
       next(",");
       white();
     }
   }
+
   error("Bad object");
 };
 
 value = function () {
   // Parse a JSON value. It could be an object, an array, a string, a number,
   // or a word.
-
   white();
+
   switch (ch) {
     case "{":
       return object();
+
     case "[":
       return array();
+
     case '"':
       return string();
+
     case "-":
       return number();
+
     default:
       return ch >= "0" && ch <= "9" ? number() : word();
   }
@@ -632,15 +618,14 @@ value = function () {
 
 // Set the New JSONBigNumber.parse function It will have access to all of the above
 // functions and variables.
-
 export function parse(source, reviver) {
   var result;
-
   text = source;
   at = 0;
   ch = " ";
   result = value();
   white();
+
   if (ch) {
     error("Syntax error");
   }
@@ -650,16 +635,17 @@ export function parse(source, reviver) {
   // transformation, starting with a temporary root object that holds the result
   // in an empty key. If there is not a reviver function, we simply return the
   // result.
-
   return typeof reviver === "function"
     ? (function walk(holder, key) {
         var k;
         var v;
         var val = holder[key];
+
         if (val && typeof val === "object") {
           for (k in val) {
             if (Object.prototype.hasOwnProperty.call(val, k)) {
               v = walk(val, k);
+
               if (v !== undefined) {
                 val[k] = v;
               } else {
@@ -668,6 +654,7 @@ export function parse(source, reviver) {
             }
           }
         }
+
         return reviver.call(holder, key, val);
       })(
         {
@@ -677,7 +664,6 @@ export function parse(source, reviver) {
       )
     : result;
 }
-
 export default {
   parse: parse,
   stringify: stringify,
