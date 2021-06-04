74f35d5b7eb116aaa0f50d9b1dfae1fe307d48d7
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-04 16:41:27 +0200

    observable.js

--- src/observable.ts
+++ src/observable.ts
@@ -1,11 +1,8 @@
-// @flow
-
 import { ReplaySubject, Observable } from "rxjs";
 import { useEffect, useState } from "react";
-
 // emit value each time it changes by reference. it replays the last value at subscribe time.
 export function useReplaySubject<T>(value: T): ReplaySubject<T> {
-  const [subject] = useState(() => new ReplaySubject());
+  const [subject] = useState(() => new ReplaySubject<any>());
   useEffect(() => {
     subject.next(value);
   }, [subject, value]);
@@ -16,12 +13,13 @@ export function useReplaySubject<T>(value: T): ReplaySubject<T> {
   }, [subject]);
   return subject;
 }
-
 export function useObservable<T>(
   observable: Observable<T>,
   initialValue?: T
-): ?T {
-  const [value, update] = useState<?T>(initialValue || undefined);
+): T | null | undefined {
+  const [value, update] = useState<T | null | undefined>(
+    initialValue || undefined
+  );
   useEffect(() => {
     const s = observable.subscribe(update);
     return () => s.unsubscribe();
