dc2232263bb36457aa5fb3b4e392ed3783f4fa8f
Thomas Nguy <thomas.nguy@crypto.com>
2021-06-07 11:34:40 +0900

    format todo string

--- src/families/crypto_org/js-getFeesForTransaction.js
+++ src/families/crypto_org/js-getFeesForTransaction.js
@@ -6,7 +6,7 @@ import { FIXED_GAS_PRICE, FIXED_DEFAULT_GAS_LIMIT } from "./logic";
  * Fetch the transaction fees for a transaction
  */
 const getEstimatedFees = async (): Promise<BigNumber> => {
-  // Todo call gas station to get a more accurate tx fee in the future
+  // TODO: call gas station to get a more accurate tx fee in the future
   let estimateFee = Math.ceil(FIXED_GAS_PRICE * FIXED_DEFAULT_GAS_LIMIT);
   return new BigNumber(estimateFee);
 };
