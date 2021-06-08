334e836292460adb1ad9e1a37f1d0c008b1718cf
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-08 14:38:32 +0200

    portfolio/v2/typesjs portfolio/range.js

--- src/portfolio/range.ts
+++ src/portfolio/range.ts
@@ -1,29 +1,21 @@
-// @flow
-
 import type { PortfolioRange, PortfolioRangeConfig } from "../types";
-
 export const hourIncrement = 60 * 60 * 1000;
 export const dayIncrement = 24 * hourIncrement;
 export const weekIncrement = 7 * dayIncrement;
-
 export function startOfHour(t: Date) {
   return new Date(t.getFullYear(), t.getMonth(), t.getDate(), t.getHours());
 }
-
 export function startOfMonth(t: Date) {
   return new Date(t.getFullYear(), t.getMonth(), 1);
 }
-
 export function startOfDay(t: Date) {
   return new Date(t.getFullYear(), t.getMonth(), t.getDate());
 }
-
 export function startOfWeek(t: Date) {
   const d = startOfDay(t);
   return new Date(d.getTime() - d.getDay() * dayIncrement);
 }
-
-const ranges: { [k: PortfolioRange]: PortfolioRangeConfig } = {
+const ranges: Record<PortfolioRange, PortfolioRangeConfig> = {
   year: {
     count: 52,
     increment: weekIncrement,
@@ -50,9 +42,10 @@ const ranges: { [k: PortfolioRange]: PortfolioRangeConfig } = {
   },
 };
 
-const getPerPortfolioRanges = (): {
-  [k: PortfolioRange]: PortfolioRangeConfig,
-} => {
+const getPerPortfolioRanges = (): Record<
+  PortfolioRange,
+  PortfolioRangeConfig
+> => {
   return ranges;
 };
 
@@ -61,18 +54,18 @@ export function getPortfolioRangeConfig(
 ): PortfolioRangeConfig {
   return getPerPortfolioRanges()[r];
 }
-
 export const getRanges = (): PortfolioRange[] =>
-  Object.keys(getPerPortfolioRanges());
-
+  Object.keys(getPerPortfolioRanges()) as PortfolioRange[];
 export function getDates(r: PortfolioRange): Date[] {
   const conf = getPortfolioRangeConfig(r);
   let t = new Date();
   const array = [t];
-  t = new Date(conf.startOf(t) - 1); // end of yesterday
+  t = new Date(conf.startOf(t).valueOf() - 1); // end of yesterday
+
   for (let d = conf.count - 1; d > 0; d--) {
     array.unshift(t);
-    t = new Date(t - conf.increment);
+    t = new Date(t.valueOf() - conf.increment);
   }
+
   return array;
 }
