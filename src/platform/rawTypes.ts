4edfa1e70b567e3dd118ae89e82129b26d3ed8a5
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-08 14:41:58 +0200

    platform/[rawTypes|types].js

--- src/platform/rawTypes.ts
+++ src/platform/rawTypes.ts
@@ -1,39 +1,32 @@
-// @flow
 import type { SignedOperationRaw } from "../types";
-
 export type RawPlatformAccount = {
-  id: string,
-  name: string,
-  address: string,
-  currency: string,
-  balance: string,
-  spendableBalance: string,
-  blockHeight: number,
-  lastSyncDate: string,
+  id: string;
+  name: string;
+  address: string;
+  currency: string;
+  balance: string;
+  spendableBalance: string;
+  blockHeight: number;
+  lastSyncDate: string;
 };
-
 export interface RawPlatformTransactionCommon {
   amount: string;
   recipient: string;
 }
-
 export interface RawPlatformEthereumTransaction
   extends RawPlatformTransactionCommon {
   family: "ethereum";
-  nonce: ?number;
-  data: ?string;
-  gasPrice: ?string;
-  gasLimit: ?string;
+  nonce: number | null | undefined;
+  data: string | null | undefined;
+  gasPrice: string | null | undefined;
+  gasLimit: string | null | undefined;
 }
-
 export interface RawPlatformBitcoinTransaction
   extends RawPlatformTransactionCommon {
   family: "bitcoin";
-  feePerByte: ?string;
+  feePerByte: string | null | undefined;
 }
-
 export type RawPlatformTransaction =
   | RawPlatformEthereumTransaction
   | RawPlatformBitcoinTransaction;
-
 export type RawPlatformSignedTransaction = SignedOperationRaw;
--- src/platform/types.ts
+++ src/platform/types.ts
@@ -1,56 +1,45 @@
-// @flow
-
 import type { BigNumber } from "bignumber.js";
-
 import type { SignedOperation } from "../types";
-
 export type PlatformAccount = {
-  id: string,
-  name: string,
-  address: string,
-  currency: string,
-  balance: BigNumber,
-  spendableBalance: BigNumber,
-  blockHeight: number,
-  lastSyncDate: Date,
+  id: string;
+  name: string;
+  address: string;
+  currency: string;
+  balance: BigNumber;
+  spendableBalance: BigNumber;
+  blockHeight: number;
+  lastSyncDate: Date;
 };
-
 export type PlatformUnit = {
-  name: string,
-  code: string,
-  magnitude: number,
+  name: string;
+  code: string;
+  magnitude: number;
 };
-
 export type PlatformCurrency = {
-  type: string,
-  color: string,
-  ticker: string,
-  id: string,
-  name: string,
-  family: string,
-  units: PlatformUnit[],
+  type: string;
+  color: string;
+  ticker: string;
+  id: string;
+  name: string;
+  family: string;
+  units: PlatformUnit[];
 };
-
 export interface PlatformTransactionCommon {
   amount: BigNumber;
   recipient: string;
 }
-
 export interface PlatformEthereumTransaction extends PlatformTransactionCommon {
   family: "ethereum";
-  nonce: ?number;
-  data: ?Buffer;
-  gasPrice: ?BigNumber;
-  gasLimit: ?BigNumber;
+  nonce: number | null | undefined;
+  data: Buffer | null | undefined;
+  gasPrice: BigNumber | null | undefined;
+  gasLimit: BigNumber | null | undefined;
 }
-
 export interface PlatformBitcoinTransaction extends PlatformTransactionCommon {
   family: "bitcoin";
-  feePerByte: ?BigNumber;
+  feePerByte: BigNumber | null | undefined;
 }
-
 export type PlatformTransaction =
   | PlatformEthereumTransaction
   | PlatformBitcoinTransaction;
-
 export type PlatformSignedTransaction = SignedOperation;
