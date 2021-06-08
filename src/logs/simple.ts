7e208bdf7808bed0c625b681f5805143899d5612
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-08 14:51:15 +0200

    logs/simple.js

--- src/logs/simple.ts
+++ src/logs/simple.ts
@@ -1,10 +1,9 @@
-// @flow
 import winston from "winston";
 import { MESSAGE } from "triple-beam";
 const { format } = winston;
-
 export default format((info) => {
   const padding = (info.padding && info.padding[info.level]) || "";
+
   if (info.data !== undefined) {
     info[MESSAGE] = `${info.level}:${padding} ${info.message} ${JSON.stringify(
       info.data
@@ -12,5 +11,6 @@ export default format((info) => {
   } else {
     info[MESSAGE] = `${info.level}:${padding} ${info.message}`;
   }
+
   return info;
 });
