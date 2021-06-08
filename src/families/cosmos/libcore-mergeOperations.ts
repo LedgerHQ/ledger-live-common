cdbb2cc6bb13dd84288d645d4c20fdc13628447e
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-08 17:18:21 +0200

    families/cosmos/libcore-mergeOperations.js

--- src/families/cosmos/libcore-mergeOperations.ts
+++ src/families/cosmos/libcore-mergeOperations.ts
@@ -1,27 +1,32 @@
-// @flow
 import invariant from "invariant";
 import type { Operation } from "../../types";
 
 const mergeOperations = (operations: Operation[]): Operation => {
   const feeOp = operations.find((op) => op.type === "FEES");
   const otherOps = operations.filter((op) => op !== feeOp);
+
   if (otherOps.length === 0) {
     invariant(feeOp, "fee op is necessary if no other ops exists");
-    return feeOp;
+    return feeOp as Operation;
   }
+
   const union: Operation = { ...otherOps[0] };
   // remove the "-0" part
   union.id = union.id.split("-").slice(0, 3).join("-");
+
   if (feeOp) {
     union.fee = feeOp.value;
+
     if (union.type === "OUT") {
       union.value = union.value.plus(feeOp.value);
     } else {
       union.value = feeOp.value;
     }
   }
+
   const extra = { ...union.extra };
   delete extra.id;
+
   // accumulate validators
   if (extra.validators) {
     extra.validators = otherOps.reduce(
@@ -30,6 +35,7 @@ const mergeOperations = (operations: Operation[]): Operation => {
       []
     );
   }
+
   union.extra = extra;
   return union;
 };
