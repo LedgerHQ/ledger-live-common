2b5a6af1a6b2053fbd1df2657a56cbaad2363fce
huiqi <huiqi.zheng@ledger.fr>
2021-06-10 10:40:21 +0200

    debug digibyte in bot

--- src/bot/index.js
+++ src/bot/index.js
@@ -58,9 +58,7 @@ export async function bot({ currency, mutation }: Arg = {}) {
       if (
         !isCurrencySupported(spec.currency) ||
         spec.disabled ||
-        (spec.currency.id != "stakenet" &&
-          spec.currency.id != "stealthcoin" &&
-          spec.currency.id != "digibyte")
+        spec.currency.id != "digibyte"
       ) {
         continue;
       }
