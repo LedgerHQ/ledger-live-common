92c111553bb719e695e35b497c5f726665036fab
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-15 17:29:57 +0200

    [cli] src/qr.js

--- cli/src/qr.ts
+++ cli/src/qr.ts
@@ -1,10 +1,7 @@
-// @flow
-
 import { Observable } from "rxjs";
 import qrcode from "qrcode-terminal";
-
-export const asQR = (str: string) =>
-  Observable.create((o) =>
+export const asQR = (str: string): Observable<string> =>
+  new Observable((o) =>
     qrcode.generate(str, (r) => {
       o.next(r);
       o.complete();
