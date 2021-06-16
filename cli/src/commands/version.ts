96c4bc9220321e490df2face06a9a09a2662dc25
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-06-16 15:33:46 +0200

    [cli] src/version.js

--- cli/src/commands/version.ts
+++ cli/src/commands/version.ts
@@ -1,22 +1,19 @@
-// @flow
 /* eslint-disable global-require */
-
 import { from, of, concat } from "rxjs";
 import { map } from "rxjs/operators";
 import { withLibcore } from "@ledgerhq/live-common/lib/libcore/access";
-
 export default {
   args: [],
-  job: () =>
+  job: async () =>
     concat(
-      of("ledger-live cli: " + require("../../package.json").version),
+      of("ledger-live cli: " + (await import("../../package.json")).version),
       of(
         "@ledgerhq/live-common: " +
-          require("@ledgerhq/live-common/package.json").version
+          (await import("@ledgerhq/live-common/package.json")).version
       ),
       of(
         "@ledgerhq/ledger-core: " +
-          require("@ledgerhq/ledger-core/package.json").version
+          (await import("@ledgerhq/ledger-core/package.json")).version
       ),
       from(withLibcore((core) => core.LedgerCore.getStringVersion())).pipe(
         map((v) => "libcore: " + v)
