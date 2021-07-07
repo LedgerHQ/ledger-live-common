32bb0a79b4ba0095fc16072518e6286210ee5b9c
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-07-07 11:03:08 +0200

    fix script to generate flags

--- scripts/build-ts.sh
+++ scripts/build-ts.sh
@@ -6,6 +6,7 @@ PATH=$(yarn bin):$PATH
 rm -rf lib src/data/icons/react*
 bash ./scripts/sync-families-dispatch.sh
 node scripts/buildReactIcons.js
+node scripts/buildReactFlags.js
 
 export NODE_ENV=production
 tsc --project src/tsconfig.json
