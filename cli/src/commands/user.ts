13fa1d76e8118f509bf5bc1154d2bcf4547496e8
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-07-20 17:36:20 +0200

    user command

--- cli/src/commands/user.ts
+++ cli/src/commands/user.ts
@@ -1,7 +1,5 @@
-// @flow
 import { of, concat } from "rxjs";
 import { getUserHashes } from "@ledgerhq/live-common/lib/user";
-
 export default {
   args: [],
   job: () => concat(of(JSON.stringify(getUserHashes()))),
