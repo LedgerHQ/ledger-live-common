57294b47d9c1c6aef6a3101455b99c3ef79a5d04
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-07-29 11:48:41 +0300

    add tests for elrond transactions

--- src/families/elrond/test-dataset.js
+++ src/families/elrond/test-dataset.js
@@ -1,5 +1,6 @@
 // @flow
 import type { DatasetTest } from "../../types";
+import { fromTransactionRaw } from "../elrond/transaction";
 import type { Transaction } from "./types";
 
 type TestTransaction = {
@@ -13,18 +14,20 @@ type TestTransaction = {
 };
 
 export default dataset = {
-  implementations: ["js"],
+  implementations: ["mock", "js"],
   currencies: {
     elrond: {
       scanAccounts: [
         {
           name: "elrond seed 1",
           apdus: `
-              => ed030000080000000000000000
-              <= 3e65726431726e71376d386b6472707966616e793466776571747964736b34706778306c3664746c367476636164357765736167727578747336326539726b9000
-              => ed030000080000000000000000
-              <= 3e65726431726e71376d386b6472707966616e793466776571747964736b34706778306c3664746c367476636164357765736167727578747336326539726b9000
-              `,
+          => ed030000080000000000000000
+          <= 3e6572643176676670336737617a716a783477736d7474373036376d306c3632763370736d717a7232346a36787679776a32746c7a3067657376797a7371329000
+          => ed030000080000000080000000
+          <= 3e6572643176676670336737617a716a783477736d7474373036376d306c3632763370736d717a7232346a36787679776a32746c7a3067657376797a7371329000
+          => ed030000080000000080000001
+          <= 3e65726431706d33676a65326c6d643576796c6463767579366a7078676465347261706a70756a76756a6b65653661376a77327a6d6834747172653739367a9000
+          `,
         },
       ],
       accounts: [
@@ -49,7 +52,69 @@ export default dataset = {
             balance: "299569965",
           },
           transactions: [
-            // HERE WE WILL INSERT OUR test
+            {
+              name: "recipient and sender must not be the same",
+              transaction: fromTransactionRaw({
+                family: "elrond",
+                recipient:
+                  "erd1rnq7m8kdrpyfany4fweqtydsk4pgx0l6dtl6tvcad5wesagruxts62e9rk",
+                amount: "100000000",
+                mode: "send",
+                era: null,
+                validators: [],
+                fees: null,
+                rewardDestination: null,
+                numSlashingSpans: 0,
+              }),
+              expectedStatus: {
+                amount: BigNumber("100000000"),
+                errors: {
+                  recipient: new InvalidAddressBecauseDestinationIsAlsoSource(),
+                },
+                warnings: {},
+              },
+            },
+            {
+              name: "Not a valid address",
+              transaction: fromTransactionRaw({
+                family: "elrond",
+                recipient: "elrondinv",
+                amount: "100000000",
+                mode: "send",
+                era: null,
+                validators: [],
+                fees: null,
+                rewardDestination: null,
+                numSlashingSpans: 0,
+              }),
+              expectedStatus: {
+                errors: {
+                  recipient: new InvalidAddress(),
+                },
+                warnings: {},
+              },
+            },
+            {
+              name: "Not enough balance",
+              transaction: fromTransactionRaw({
+                family: "elrond",
+                recipient:
+                  "erd1frj909pfums4m8aza596595l9pl56crwdj077vs2aqcw6ynl28wsfkw9rd",
+                amount: "1000000000000000",
+                mode: "send",
+                era: null,
+                validators: [],
+                fees: null,
+                rewardDestination: null,
+                numSlashingSpans: 0,
+              }),
+              expectedStatus: {
+                errors: {
+                  amount: new NotEnoughBalance(),
+                },
+                warnings: {},
+              },
+            },
           ],
         },
       ],
