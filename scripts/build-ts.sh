61733f4c01f3e4cb269bc6c75f2ba2a4dcab0844
MortalKastor <thibaut@kastor.fr>
2021-08-05 11:21:30 +0200

    LL-6568 [Windows] Fix tsc failing to run

--- scripts/build-ts.sh
+++ scripts/build-ts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-PATH=$(yarn bin):$PATH
 
 rm -rf lib src/data/icons/react*
 bash ./scripts/sync-families-dispatch.sh
@@ -9,7 +8,7 @@ node scripts/buildReactIcons.js
 node scripts/buildReactFlags.js
 
 export NODE_ENV=production
-tsc --project src/tsconfig.json
+yarn tsc --project src/tsconfig.json
 
 
 # (
--- scripts/watch-ts.sh
+++ scripts/watch-ts.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 set -e
-PATH=$(yarn bin):$PATH
 
 rm -rf lib src/data/icons/react*
 bash ./scripts/sync-families-dispatch.sh
 node scripts/buildReactIcons.js
 
-tsc --watch
+yarn tsc --watch
 
 # (
 #     cd ../../flow-support
