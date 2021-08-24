21df5ec01e1cdc7faba1ead1b09349a41e2756e2
klambert-ledger <kevin.lambert@ledger.fr>
2021-08-24 11:54:04 +0200

    Update Eth API and sync to handle NFTs

--- src/api/Ethereum.ts
+++ src/api/Ethereum.ts
@@ -1,9 +1,10 @@
+import setWith from "lodash/setWith";
 import URL from "url";
 import invariant from "invariant";
 import { BigNumber } from "bignumber.js";
 import { LedgerAPINotAvailable } from "@ledgerhq/errors";
 import JSONBigNumber from "@ledgerhq/json-bignumber";
-import type { CryptoCurrency } from "../types";
+import type { CryptoCurrency, NFTMetadataResponse } from "../types";
 import type { EthereumGasLimitRequest } from "../families/ethereum/types";
 import network from "../network";
 import { blockchainBaseURL } from "./Ledger";
@@ -37,6 +38,12 @@ export type Tx = {
     }>;
     truncated: boolean;
   };
+  erc721_transfer_events?: Array<{
+    contract: string;
+    sender: string;
+    receiver: string;
+    token_id: string;
+  }>;
   actions?: Array<{
     from: string;
     to: string;
@@ -59,6 +66,10 @@ export type ERC20BalanceOutput = Array<{
   contract: string;
   balance: BigNumber;
 }>;
+export type NFTMetadataInput = Array<{
+  contract: string;
+  tokenId: string;
+}>;
 export type API = {
   getTransactions: (
     address: string,
@@ -72,6 +83,7 @@ export type API = {
   getAccountNonce: (address: string) => Promise<number>;
   broadcastTransaction: (signedTransaction: string) => Promise<string>;
   getERC20Balances: (input: ERC20BalancesInput) => Promise<ERC20BalanceOutput>;
+  getNFTMetadata: (input: NFTMetadataInput) => Promise<NFTMetadataResponse[]>;
   getAccountBalance: (address: string) => Promise<BigNumber>;
   roughlyEstimateGasLimit: (address: string) => Promise<BigNumber>;
   getERC20ApprovalsPerContract: (
@@ -175,6 +187,16 @@ export const apiForCurrency = (currency: CryptoCurrency): API => {
         transformResponse: JSONBigNumber.parse,
         data: input,
       });
+      return data;
+    },
+
+    async getNFTMetadata(input) {
+      const { data }: { data: NFTMetadataResponse[] } = await network({
+        method: "POST",
+        url: "https://mock-nft-metadata-api.vercel.app/v1/chains/eth/contracts/tokens/infos",
+        data: input,
+      });
+
       return data;
     },
 
--- src/families/ethereum/synchronisation.ts
+++ src/families/ethereum/synchronisation.ts
@@ -15,10 +15,11 @@ import {
 } from "../../account";
 import { listTokensForCryptoCurrency } from "../../currencies";
 import { encodeAccountId } from "../../account";
-import type { Operation, TokenAccount, Account } from "../../types";
+import type { Operation, TokenAccount, Account, NFT } from "../../types";
 import { API, apiForCurrency, Tx } from "../../api/Ethereum";
 import { digestTokenAccounts, prepareTokenAccounts } from "./modules";
 import { findTokenByAddressInCurrency } from "@ledgerhq/cryptoassets";
+import { encodeNftId } from "../../nft";
 export const getAccountShape: GetAccountShape = async (
   infoInput,
   { blacklistedTokenIds }
@@ -75,8 +76,10 @@ export const getAccountShape: GetAccountShape = async (
   let newOps = flatMap(txs, txToOps({ address, id: accountId, currency }));
   // extracting out the sub operations by token account
   const perTokenAccountIdOperations = {};
+  // extracting and concat all nft operations for an account
+  const flatNftOps: Operation[] = [];
   newOps.forEach((op) => {
-    const { subOperations } = op;
+    const { subOperations, nftOperations } = op;
 
     if (subOperations?.length) {
       subOperations.forEach((sop) => {
@@ -86,6 +89,10 @@ export const getAccountShape: GetAccountShape = async (
 
         perTokenAccountIdOperations[sop.accountId].push(sop);
       });
+    }
+
+    if (nftOperations?.length) {
+      nftOperations.forEach((nop) => flatNftOps.push(nop));
     }
   });
   const subAccountsExisting = {};
@@ -158,6 +165,8 @@ export const getAccountShape: GetAccountShape = async (
     subOperations: inferSubOperations(o.hash, subAccounts),
   }));
   const operations = mergeOps(initialStableOperations, newOps);
+
+  const nfts = await getERC721Nfts(flatNftOps);
   const accountShape: Partial<Account> = {
     id: accountId,
     operations,
@@ -168,6 +177,7 @@ export const getAccountShape: GetAccountShape = async (
     lastSyncDate: new Date(),
     balanceHistory: undefined,
     syncHash,
+    nfts,
   };
   return accountShape;
 };
@@ -188,7 +198,8 @@ const txToOps =
   (tx: Tx): Operation[] => {
     // workaround bugs in our explorer that don't treat partial/optimistic operation really well
     if (!tx.gas_used) return [];
-    const { hash, block, actions, transfer_events } = tx;
+    const { hash, block, actions, transfer_events, erc721_transfer_events } =
+      tx;
     const addr = address;
     const from = safeEncodeEIP55(tx.from);
     const to = safeEncodeEIP55(tx.to);
@@ -305,6 +316,71 @@ const txToOps =
 
           return all;
         });
+    // Creating NFTOps from transfer events related to ERC721 only
+    const nftOperations = !erc721_transfer_events
+      ? []
+      : flatMap(erc721_transfer_events, (event) => {
+          const sender = safeEncodeEIP55(event.sender);
+          const receiver = safeEncodeEIP55(event.receiver);
+          const contract = safeEncodeEIP55(event.contract);
+          const tokenId = event.token_id;
+          const nftId = encodeNftId(id, event.contract, tokenId);
+          const sending = addr === sender;
+          const receiving = addr === receiver;
+
+          if (!sending && !receiving) {
+            return [];
+          }
+
+          const all: Operation[] = [];
+
+          if (sending) {
+            const type = "NFT_OUT";
+            all.push({
+              id: `${nftId}-${hash}-${type}`,
+              senders: [sender],
+              recipients: [receiver],
+              contract,
+              fee,
+              standard: "ERC721",
+              tokenId,
+              value: new BigNumber(1),
+              hash,
+              type,
+              blockHeight,
+              blockHash,
+              date,
+              transactionSequenceNumber,
+              accountId: id,
+              extra: {},
+            });
+          }
+
+          if (receiving) {
+            const type = "NFT_IN";
+            all.push({
+              id: `${nftId}-${hash}-${type}`,
+              senders: [sender],
+              recipients: [receiver],
+              contract,
+              fee,
+              standard: "ERC721",
+              tokenId,
+              value: new BigNumber(1),
+              hash,
+              type,
+              blockHeight,
+              blockHash,
+              date,
+              transactionSequenceNumber,
+              accountId: id,
+              extra: {},
+            });
+          }
+
+          return all;
+        });
+
     const ops: Operation[] = [];
 
     if (sending) {
@@ -325,6 +401,7 @@ const txToOps =
         hasFailed,
         internalOperations: internalOperations,
         subOperations,
+        nftOperations,
         transactionSequenceNumber,
       });
     }
@@ -347,6 +424,7 @@ const txToOps =
         internalOperations: sending ? [] : internalOperations,
         // if it was already in sending, we don't add twice
         subOperations: sending ? [] : subOperations,
+        nftOperations,
         transactionSequenceNumber,
       });
     }
@@ -354,7 +432,9 @@ const txToOps =
     if (
       !sending &&
       !receiving &&
-      (internalOperations.length || subOperations.length)
+      (internalOperations.length ||
+        subOperations.length ||
+        nftOperations.length)
     ) {
       ops.push({
         id: `${id}-${hash}-NONE`,
@@ -371,6 +451,7 @@ const txToOps =
         extra: {},
         internalOperations,
         subOperations,
+        nftOperations,
         transactionSequenceNumber,
       });
     }
@@ -443,6 +524,50 @@ async function loadERC20Balances(tokenAccounts, address, api) {
       return a;
     })
     .filter(Boolean);
+}
+
+function getERC721Nfts(nftOperations: Operation[]): NFT[] {
+  const nftOpsMap: Record<string, Operation[]> = nftOperations.reduce(
+    (acc, op) => {
+      // Creating a "token for a contract" unique key
+      const nftKey = op.contract! + op.tokenId!;
+
+      if (!acc[nftKey]) {
+        acc[nftKey] = [];
+      }
+
+      acc[nftKey].push(op);
+      return acc;
+    },
+    {}
+  );
+
+  const NFTs = Object.keys(nftOpsMap)
+    .map((n) => {
+      const lastOp = nftOpsMap[n]
+        .sort((a, b) => b.date.getTime() - a.date.getTime())
+        .shift();
+
+      if (lastOp?.type === "IN") {
+        const contract = lastOp.contract?.toLowerCase?.();
+        const { tokenId } = lastOp;
+
+        return {
+          id: lastOp.id,
+          tokenId,
+          // ERC721 is non fungible, so amount will always be 1
+          amount: new BigNumber(1),
+          collection: {
+            contract,
+            standard: "ERC721",
+          },
+        } as NFT;
+      }
+      return null;
+    })
+    .filter(Boolean) as NFT[];
+
+  return NFTs;
 }
 
 const SAFE_REORG_THRESHOLD = 80;
