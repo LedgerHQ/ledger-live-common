a7931534555f12822faba170f217f2f18f6737a9
haammar-ledger <hakim.aammar-ext@ledger.fr>
2021-09-10 18:39:23 +0200

    Update generated files for libcore impl

--- src/generated/bridge/libcore.ts
+++ src/generated/bridge/libcore.ts
@@ -1,5 +1,7 @@
 import algorand from "../../families/algorand/bridge/libcore";
 
+import bitcoin from "../../families/bitcoin/bridge/libcore";
+
 import cosmos from "../../families/cosmos/bridge/libcore";
 
 import tezos from "../../families/tezos/bridge/libcore";
@@ -7,6 +9,7 @@ import tezos from "../../families/tezos/bridge/libcore";
 
 export default {
   algorand,
+  bitcoin,
   cosmos,
   tezos,
 };
--- src/generated/libcore-getFeesForTransaction.ts
+++ src/generated/libcore-getFeesForTransaction.ts
@@ -1,5 +1,7 @@
 import algorand from "../families/algorand/libcore-getFeesForTransaction";
 
+import bitcoin from "../families/bitcoin/libcore-getFeesForTransaction";
+
 import cosmos from "../families/cosmos/libcore-getFeesForTransaction";
 
 import tezos from "../families/tezos/libcore-getFeesForTransaction";
@@ -7,6 +9,7 @@ import tezos from "../families/tezos/libcore-getFeesForTransaction";
 
 export default {
   algorand,
+  bitcoin,
   cosmos,
   tezos,
 };
