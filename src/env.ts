430bdfa831aaaf4bd77656be5693fdd163fd8f1d
klambert-ledger <kevin.lambert@ledger.fr>
2021-09-24 21:50:59 +0200

    Make NFT optional with env

--- src/env.ts
+++ src/env.ts
@@ -366,6 +366,11 @@ const envDefinitions = {
     parser: stringParser,
     desc: "switch the app into a MOCK mode for test purpose, the value will be used as a seed for the rng. Avoid falsy values.",
   },
+  NFT: {
+    def: false,
+    parser: boolParser,
+    desc: "synchronizing nfts",
+  },
   OPERATION_ADDRESSES_LIMIT: {
     def: 100,
     parser: intParser,
