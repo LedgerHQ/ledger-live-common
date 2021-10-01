5e154a6fe008ab4440af02a5b1f3f6af62ede534
Emmanuel <emmanuelm41@gmail.com>
2021-10-01 14:32:01 -0300

    add verify flag on getAddress func

--- src/families/filecoin/hw-getAddress.ts
+++ src/families/filecoin/hw-getAddress.ts
@@ -4,12 +4,15 @@ import { log } from "@ledgerhq/logs";
 import type { Resolver } from "../../hw/getAddress/types";
 import { getPath, isError } from "./utils";
 
-const resolver: Resolver = async (transport, { path }) => {
+const resolver: Resolver = async (transport, { path, verify }) => {
   log("debug", "start getAddress process");
 
   const fil = new Fil(transport);
 
-  const r = await fil.getAddressAndPubKey(getPath(path));
+  const r = verify
+    ? await fil.showAddressAndPubKey(getPath(path))
+    : await fil.getAddressAndPubKey(getPath(path));
+
   isError(r);
 
   return {
