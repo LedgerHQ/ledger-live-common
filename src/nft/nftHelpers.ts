9e0b0db20e6377dc7b70e556e6e9654dc741ad7c
klambert-ledger <kevin.lambert@ledger.fr>
2021-10-04 16:25:43 +0200

    Add nftsFromOperations to NFT helpers

--- src/nft/nftHelpers.ts
+++ src/nft/nftHelpers.ts
@@ -1,4 +1,6 @@
-import { NFT, NFTWithMetadata } from "../types";
+import eip55 from "eip55";
+import BigNumber from "bignumber.js";
+import { NFT, NFTWithMetadata, Operation } from "../types";
 
 type AnyNFT = NFT | NFTWithMetadata;
 type Collection = NFT["collection"] | NFTWithMetadata["collection"];
@@ -7,6 +9,44 @@ type CollectionMap<C> = Record<string, C>;
 
 export type CollectionWithNFT = Collection & {
   nfts: Array<Omit<AnyNFT, "collection">>;
+};
+
+export const nftsFromOperations = (ops: Operation[]): NFT[] => {
+  const nftsMap = ops
+    // if ops are Operations get the prop nftOperations, else ops are considered nftOperations already
+    .flatMap((op) => (op?.nftOperations?.length ? op.nftOperations : op))
+    .reduce((acc: Record<string, NFT>, nftOp: Operation) => {
+      if (!nftOp?.contract) {
+        return acc;
+      }
+
+      // Creating a "token for a contract" unique key
+      const contract = eip55.encode(nftOp.contract!);
+      const nftKey = contract + nftOp.tokenId!;
+      const { tokenId, standard, id } = nftOp;
+
+      const nft = (acc[nftKey] ?? {
+        id,
+        tokenId: tokenId!,
+        amount: new BigNumber(0),
+        collection: {
+          contract,
+          standard: standard!,
+        },
+      }) as NFT;
+
+      if (nftOp.type === "NFT_IN") {
+        nft.amount = nft.amount.plus(nftOp.value);
+      } else if (nftOp.type === "NFT_OUT") {
+        nft.amount = nft.amount.minus(nftOp.value);
+      }
+
+      acc[nftKey] = nft;
+
+      return acc;
+    }, {});
+
+  return Object.values(nftsMap);
 };
 
 export const nftsByCollections = (
