71ce299949f991c4b35089df29e13ea9ef4ce0b0
klambert-ledger <kevin.lambert@ledger.fr>
2021-10-06 20:08:36 +0200

    Add refreshNft callback to useNfts' hook return

--- src/nft/useNfts.ts
+++ src/nft/useNfts.ts
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 import { nftsFromOperations } from "./nftHelpers";
 import { API, apiForCurrency, NFTMetadataInput } from "../api/Ethereum";
 import { Operation, NFT, NFTWithMetadata, NFTMetadata } from "../types";
+import { mergeNfts } from "../bridge/jsHelpers";
+
+type AnyNFT = NFT | NFTWithMetadata;
 
 const currency: Currency = findCryptoCurrencyById("ethereum")!;
 const api: API = apiForCurrency(currency);
@@ -14,9 +17,34 @@ const metadataLoader = new Dataloader(
   { cacheKeyFn: ({ contract, tokenId }) => contract + tokenId }
 );
 
+const mergeNftWithMetadata = (
+  nft: NFT,
+  metadata: NFTMetadata
+): NFTWithMetadata => {
+  if (!nft || !metadata) {
+    return {} as NFTWithMetadata;
+  }
+
+  return {
+    id: nft.id,
+    tokenId: nft.tokenId,
+    amount: nft.amount,
+    nftName: metadata.nftName,
+    picture: metadata.picture,
+    description: metadata.description,
+    properties: metadata.properties,
+    collection: {
+      contract: nft.collection.contract,
+      standard: nft.collection.standard,
+      tokenName: metadata.tokenName,
+    },
+  } as NFTWithMetadata;
+};
+
 export const overloadNfts = async (
-  nftsOrOps: NFT[] | Operation[]
+  _nftsOrOps: NFT | NFT[] | Operation | Operation[]
 ): Promise<NFTWithMetadata[]> => {
+  const nftsOrOps = Array.isArray(_nftsOrOps) ? _nftsOrOps : [_nftsOrOps];
   // if entry has a hash it's an NFT Operation, not an NFT
   const nfts = (nftsOrOps[0] as Operation)?.hash
     ? nftsFromOperations(nftsOrOps as Operation[])
@@ -24,6 +52,7 @@ export const overloadNfts = async (
 
   const NFTMetadata = await Promise.all(
     nfts
+      .filter(Boolean)
       .map(({ collection: { contract }, tokenId }) => ({
         contract,
         tokenId,
@@ -32,39 +61,56 @@ export const overloadNfts = async (
   );
 
   return (nfts || []).map((nft: NFT, index) => {
-    const md = (
+    const metadata = (
       !(NFTMetadata instanceof Error) ? NFTMetadata[index] : {}
     ) as NFTMetadata;
 
-    return {
-      id: nft.id,
-      tokenId: nft.tokenId,
-      amount: nft.amount,
-      nftName: md.nftName,
-      picture: md.picture,
-      description: md.description,
-      properties: md.properties,
-      collection: {
-        contract: nft.collection.contract,
-        standard: nft.collection.standard,
-        tokenName: md.tokenName,
-      },
-    } as NFTWithMetadata;
+    return mergeNftWithMetadata(nft, metadata);
   });
 };
 
 /**
- * Hook to get NFTs overloaded with the metadata
- *
- * @param nftsOrOps @warning This need to be either a memoized variable or a static reference or you will create a memory leak
- * @returns {NFT[]}
+ * @warning ⚠️ nftsOrOps is going to be the dependency of a useEffect,
+ * therefore it needs to be reference or a memoized value or it will lead to a memory leak ⚠️
  */
-export const useNfts = (nftsOrOps: NFT[] | Operation[]): NFTWithMetadata[] => {
+export const useNfts = (
+  nftsOrOps: NFT | NFT[] | Operation | Operation[]
+): [NFTWithMetadata | NFTWithMetadata[], typeof refreshNft] => {
   const [overloadedNfts, setOverloadedNfts] = useState<NFTWithMetadata[]>([]);
+
+  async function refreshNft(): Promise<void>;
+  async function refreshNft(nftOrNfts: AnyNFT | AnyNFT[]): Promise<void>;
+  async function refreshNft(nftOrNfts?: AnyNFT | AnyNFT[]): Promise<void> {
+    const nftsToRefresh: AnyNFT[] = (() => {
+      if (!nftOrNfts) {
+        return overloadedNfts;
+      }
+
+      return Array.isArray(nftOrNfts) ? nftOrNfts : [nftOrNfts];
+    })();
+
+    const refreshedMetadata: NFTMetadata[] = await Promise.all(
+      nftsToRefresh
+        .map(({ collection: { contract }, tokenId }) => ({
+          contract,
+          tokenId,
+        }))
+        .map((c) => metadataLoader.clear(c).load(c))
+    );
+
+    const newNfts = nftsToRefresh.map((nft, i) =>
+      mergeNftWithMetadata(nft, refreshedMetadata[i])
+    );
+
+    setOverloadedNfts(mergeNfts(overloadedNfts, newNfts) as NFTWithMetadata[]);
+  }
 
   useEffect(() => {
     overloadNfts(nftsOrOps).then(setOverloadedNfts);
   }, [nftsOrOps]);
 
-  return overloadedNfts;
+  return [
+    Array.isArray(nftsOrOps) ? overloadedNfts : overloadedNfts[0],
+    refreshNft,
+  ];
 };
