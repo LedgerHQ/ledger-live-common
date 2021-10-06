fef2193184efd499f390088a61094a951b40d883
LFBarreto <lfb.access@gmail.com>
2021-10-06 15:20:16 +0200

    (Swap): usePickDefaultAccounts fix review

--- src/exchange/swap/hooks/usePickDefaultAccount.ts
+++ src/exchange/swap/hooks/usePickDefaultAccount.ts
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { Account, TokenAccount } from "../../../types";
-import { BigNumber } from "bignumber.js";
 
 // Pick a default source account if none are selected.
 export const usePickDefaultAccount = (
@@ -10,39 +9,12 @@ export const usePickDefaultAccount = (
 ): void => {
   useEffect(() => {
     if (!fromAccount) {
-      const possibleDefaults = accounts.reduce<
-        [
-          Account | TokenAccount | undefined | null,
-          Account | TokenAccount | undefined | null,
-          Account | TokenAccount | undefined | null
-        ]
-      >(
-        (acc, account) => {
-          if (account.disabled) return acc;
-          if (
-            // eventually needs a type guard because the "currency" property does not exist in TokenAccount
-            account["currency"]?.id === "ethereum" &&
-            (acc[0]?.balance ?? new BigNumber(-1)).lt(account.balance)
-          ) {
-            acc[0] = account;
-          }
-          if (
-            // eventually needs a type guard because the "currency" property does not exist in TokenAccount
-            account["currency"]?.id === "bitcoin" &&
-            (acc[1]?.balance ?? new BigNumber(-1)).lt(account.balance)
-          ) {
-            acc[1] = account;
-          }
-          const maxFundsAccount = acc[2];
-          if (!maxFundsAccount || maxFundsAccount.balance.lt(account.balance)) {
-            acc[2] = account;
-          }
-          return acc;
-        },
-        [null, null, null]
-      );
-      const defaultAccount = possibleDefaults.find(Boolean);
-      defaultAccount && setFromAccount(defaultAccount);
+      const defaultAccount: Account | TokenAccount | undefined = accounts
+        .filter((account) => !account.disabled && account.balance.gt(0))
+        .sort((a, b) => a.balance.minus(b.balance).toNumber())
+        .find(Boolean);
+
+      if (defaultAccount) setFromAccount(defaultAccount);
     }
   }, [accounts, fromAccount, setFromAccount]);
 };
