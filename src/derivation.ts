cdef43771b57184bcf66d1e1828ad4916a16b2f2
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-10-08 11:11:59 +0200

    Log to include full url when axios uses 'baseURL'

--- src/derivation.ts
+++ src/derivation.ts
@@ -445,6 +445,13 @@ export const getDerivationModesForCurrency = (
     all.push("legacy_on_native_segwit");
   }
 
+  // taproot logic. FIXME should move per family
+  if (currency.family === "bitcoin") {
+    if (currency.id === "bitcoin_testnet") {
+      all.push("taproot");
+    }
+  }
+
   if (currency.supportsNativeSegwit) {
     all.push("native_segwit");
   }
