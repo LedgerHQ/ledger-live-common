d97a4d58f5f36e31de8dd7465f277da3c007c80d
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-10-12 11:11:43 +0300

    remove incremental synchronisation

--- src/families/elrond/js-synchronisation.ts
+++ src/families/elrond/js-synchronisation.ts
@@ -15,9 +15,7 @@ const getAccountShape: GetAccountShape = async (info) => {
   });
   const oldOperations = initialAccount?.operations || [];
   // Needed for incremental synchronisation
-  const startAt = oldOperations.length
-    ? oldOperations[0].date.getTime() / 1000 + 1
-    : 0;
+  const startAt = 0;
 
   // get the current account balance state depending your api implementation
   const { blockHeight, balance, nonce } = await getAccount(address);
