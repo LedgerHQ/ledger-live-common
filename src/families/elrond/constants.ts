f7804cdb84e4519c14bbc8949b9b26862ece7c6b
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-11-18 11:07:06 +0200

    define max pagination size constant

--- src/families/elrond/constants.ts
+++ src/families/elrond/constants.ts
@@ -3,5 +3,5 @@ export const HASH_TRANSACTION = {
   options: 1,
 };
 export const METACHAIN_SHARD = 4294967295;
-export const TRANSACTIONS_SIZE = 10000;
+export const MAX_PAGINATION_SIZE = 10000;
 export const ESDT_TRANSFER_GAS = 600000;
