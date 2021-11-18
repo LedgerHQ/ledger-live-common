06b0c306eba6229930a2eb70676dd011b71af270
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-11-18 12:44:35 +0200

    set transaction amount for esdt transfers

--- src/families/elrond/js-buildTransaction.ts
+++ src/families/elrond/js-buildTransaction.ts
@@ -24,8 +24,11 @@ export const buildTransaction = async (
 
   if (ta) {
     t.data = encodeESDTTransfer(t, ta);
+    t.amount = new BigNumber(0); //amount of EGLD to be sent should be 0 in an ESDT transfer
     gasLimit = ESDT_TRANSFER_GAS; //gasLimit for and ESDT transfer
-    transactionValue = new BigNumber(0); //amount of EGLD to be sent should be 0 in an ESDT transfer
+
+    transactionValue = t.useAllAmount
+    ? ta.balance : t.amount;
   }
   else {
     transactionValue = t.useAllAmount
