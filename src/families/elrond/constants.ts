1e978f61541e54772f54e189453ec1741b771e85
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-11-18 12:44:59 +0200

    define chain id constant

--- src/families/elrond/constants.ts
+++ src/families/elrond/constants.ts
@@ -5,3 +5,4 @@ export const HASH_TRANSACTION = {
 export const METACHAIN_SHARD = 4294967295;
 export const MAX_PAGINATION_SIZE = 10000;
 export const ESDT_TRANSFER_GAS = 600000;
+export const CHAIN_ID = 'T'
