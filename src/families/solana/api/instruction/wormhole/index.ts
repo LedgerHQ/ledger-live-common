e2b67fe67e63749723d2738573b955fabac511a4
konoart <konoart@solana.com>
2021-11-23 15:57:22 +0100

    solana delete unused instructions

--- src/families/solana/api/instruction/wormhole/index.ts
+++
@@ -1,9 +0,0 @@
-import { TransactionInstruction } from "@solana/web3.js";
-
-export { isWormholeInstruction, parsWormholeInstructionTitle } from "./types";
-
-export const parseWormholeInstruction = (
-  transactionIx: TransactionInstruction
-) => {
-  return undefined;
-};
