cf1927d29e4190fc502b51c64ca06766c6ce8ff3
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-12-03 17:49:14 +0100

    update tests to simplify

--- src/families/ethereum/nft.integration.test.ts
+++ src/families/ethereum/nft.integration.test.ts
@@ -7,7 +7,7 @@ import { makeBridgeCacheSystem } from "../../bridge/cache";
 import { patchAccount } from "../../reconciliation";
 import { setEnv } from "../../env";
 
-jest.setTimeout(120000);
+jest.setTimeout(200000);
 
 const ethTestingAccounts: AccountRaw[] = [
   {
@@ -28,27 +28,8 @@ const ethTestingAccounts: AccountRaw[] = [
     lastSyncDate: "",
     xpub: "",
   },
-  {
-    id: "js:1:ethereum:0xd07dc4262bcdbf85190c01c996b4c06a461d2430:",
-    seedIdentifier: "0xd07dc4262bcdbf85190c01c996b4c06a461d2430",
-    name: "kvn",
-    derivationMode: "",
-    index: 0,
-    freshAddress: "0xd07dc4262bcdbf85190c01c996b4c06a461d2430",
-    freshAddressPath: "44'/60'/0'/0/0",
-    freshAddresses: [],
-    pendingOperations: [],
-    operations: [],
-    currencyId: "ethereum",
-    unitMagnitude: 18,
-    balance: "",
-    blockHeight: 0,
-    lastSyncDate: "",
-    xpub: "",
-  },
 ];
 
-/*
 const gaspardPolygonAccount: AccountRaw = {
   id: "js:1:polygon:0xb98d10d9f6d07ba283bfd21b2dfec050f9ae282a:",
   seedIdentifier: "0xb98d10d9f6d07ba283bfd21b2dfec050f9ae282a",
@@ -67,7 +48,6 @@ const gaspardPolygonAccount: AccountRaw = {
   lastSyncDate: "",
   xpub: "",
 };
-*/
 
 const gaspardBscAccount: AccountRaw = {
   id: "js:1:bsc:0xb98d10d9f6d07ba283bfd21b2dfec050f9ae282a:",
@@ -124,23 +104,18 @@ ethTestingAccounts.forEach((a) => {
       expect(account.nfts?.length || 0).not.toBe(0);
     });
 
-    test("remove half NFTs will restore them with operations still here", async () => {
-      const copy = {
-        ...account,
-        nfts: account.nfts?.slice(Math.ceil((account.nfts?.length || 0) / 2)),
-      };
-      const resync = await sync(copy);
-      expect(resync.nfts).toEqual(account.nfts);
+    test("no nft should be on quantity 0 or negative", () => {
+      expect(account.nfts?.find((n) => !n.amount.gt(0))).toEqual(undefined);
     });
 
-    test("remove half NFTs will restore them with missing operations too", async () => {
+    test("patchAccount restore new NFTs correctly", () => {
       const copy = {
         ...account,
         operations: [],
         nfts: account.nfts?.slice(Math.ceil((account.nfts?.length || 0) / 2)),
       };
-      const resync = await sync(copy);
-      expect(resync.nfts).toEqual(account.nfts);
+      const newAccount = patchAccount(copy, toAccountRaw(account));
+      expect(newAccount.nfts).toEqual(account.nfts);
     });
 
     test("remove half NFTs will restore them with half operations", async () => {
@@ -152,16 +127,6 @@ ethTestingAccounts.forEach((a) => {
       };
       const resync = await sync(copy);
       expect(resync.nfts).toEqual(account.nfts);
-    });
-
-    test("patchAccount restore new NFTs correctly", async () => {
-      const copy = {
-        ...account,
-        operations: [],
-        nfts: account.nfts?.slice(Math.ceil((account.nfts?.length || 0) / 2)),
-      };
-      const newAccount = patchAccount(copy, toAccountRaw(account));
-      expect(newAccount.nfts).toEqual(account.nfts);
     });
 
     test("start account with .nfts and disable the NFT flag should make it disappear", async () => {
@@ -177,16 +142,10 @@ ethTestingAccounts.forEach((a) => {
       expect(second.nfts).not.toBeFalsy();
       expect(second.nfts).toEqual(account.nfts);
     });
-
-    test("no nft should be on quantity 0 or negative", async () => {
-      expect(account.nfts?.find((n) => !n.amount.gt(0))).toEqual(undefined);
-    });
   });
 });
 
-/*
-// this is never ending here... have to disable this test... (backend issue)
-describe("gaspard NFT on polygon", () => {
+describe.skip("gaspard NFT on polygon", () => {
   let account = fromAccountRaw(gaspardPolygonAccount);
   test("first sync", async () => {
     await cache.prepareCurrency(account.currency);
@@ -196,7 +155,6 @@ describe("gaspard NFT on polygon", () => {
     expect(account.nfts).toBeFalsy();
   });
 });
-*/
 
 describe("gaspard NFT on bsc", () => {
   let account = fromAccountRaw(gaspardBscAccount);
