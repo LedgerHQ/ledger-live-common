f4d85fea387b68c3ee36e562f3880df87d8e1ba1
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-12-10 15:12:05 +0100

    ts

--- src/families/tezos/bridge/libcore.ts
+++ src/families/tezos/bridge/libcore.ts
@@ -90,6 +90,7 @@ const createTransaction = (): Transaction => ({
   networkInfo: null,
   useAllAmount: false,
   taquitoError: null,
+  totalCost: null,
 });
 
 const updateTransaction = (t, patch) => ({ ...t, ...patch });
