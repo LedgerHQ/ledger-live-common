6e471ad8f0e8dbe451309f56bbac79fcbfcae657
huiqi <huiqi.zheng@ledger.fr>
2022-01-11 16:08:21 +0100

    fix test case

--- src/__tests__/families/bitcoin/wallet-btc/xpub.synced.unit.test.ts
+++ src/__tests__/families/bitcoin/wallet-btc/xpub.synced.unit.test.ts
@@ -34,7 +34,7 @@ describe("synced xpub utilites functions", () => {
 
     beforeAll(async () => {
       await xpub.sync();
-    }, 30000);
+    }, 60000);
 
     it("should compute accounts/addresses/balances correctly", async () => {
       const addresses = await xpub.getXpubAddresses();
