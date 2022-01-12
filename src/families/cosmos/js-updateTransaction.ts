abd7953fd352466baca7f26d0c23358a7067d6e1
Alexandre Alouit <alexandre.alouit@gmail.com>
2022-01-12 15:00:57 +0100

    Update js-updateTransaction.ts
    more complex updateTransaction

--- src/families/cosmos/js-updateTransaction.ts
+++ src/families/cosmos/js-updateTransaction.ts
@@ -1,3 +1,16 @@
-const updateTransaction = (t, patch) => ({ ...t, ...patch });
+const updateTransaction = (t, patch) => {
+  if ("mode" in patch && patch.mode !== t.mode) {
+    return { ...t, ...patch, gas: null, fees: null };
+  }
+
+  if (
+    "validators" in patch &&
+    patch.validators.length !== t.validators.length
+  ) {
+    return { ...t, ...patch, gas: null, fees: null };
+  }
+
+  return { ...t, ...patch };
+};
 
 export default updateTransaction;
