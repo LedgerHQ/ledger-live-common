0422023bdad28738923916b60ddb04da8890a390
Alexandre Alouit <alexandre.alouit@gmail.com>
2022-01-13 14:58:51 +0100

    Update js-estimateMaxSpendable.ts
    fix estimateMaxSpendable

--- src/families/cosmos/js-estimateMaxSpendable.ts
+++ src/families/cosmos/js-estimateMaxSpendable.ts
@@ -1,13 +1,33 @@
+import { getAbandonSeedAddress } from "@ledgerhq/cryptoassets";
 import { BigNumber } from "bignumber.js";
 import { getMainAccount } from "../../account";
+import type { Account, AccountLike } from "../../types";
+import createTransaction from "./js-createTransaction";
+import getTransactionStatus from "./js-getTransactionStatus";
+import prepareTransaction from "./js-prepareTransaction";
+import type { Transaction } from "./types";
 
-const estimateMaxSpendable = ({ account, parentAccount, transaction }) => {
+const estimateMaxSpendable = async ({
+  account,
+  parentAccount,
+  transaction,
+}: {
+  account: AccountLike;
+  parentAccount: Account;
+  transaction: Transaction;
+}): Promise<BigNumber> => {
   const mainAccount = getMainAccount(account, parentAccount);
-  const estimatedFees = transaction?.fees || new BigNumber(5000);
 
-  return Promise.resolve(
-    BigNumber.max(0, mainAccount.balance.minus(estimatedFees))
-  );
+  const t = await prepareTransaction(mainAccount, {
+    ...createTransaction(),
+    ...transaction,
+    recipient:
+      transaction?.recipient || getAbandonSeedAddress(mainAccount.currency.id),
+    useAllAmount: true,
+  });
+
+  const s = await getTransactionStatus(mainAccount, t);
+  return s.amount;
 };
 
 export default estimateMaxSpendable;
