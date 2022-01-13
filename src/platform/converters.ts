5157f59d434d12defb923b72d62e79dbdd51569f
Hugo Morosini <hugo.morosini@gmail.com>
2022-01-13 11:19:37 +0100

    added platform ERC20 token logic

--- src/platform/converters.ts
+++ src/platform/converters.ts
@@ -1,13 +1,39 @@
 import byFamily from "../generated/platformAdapter";
 
-import type { Account, CryptoCurrency, Transaction } from "../types";
+import type {
+  Account,
+  CryptoCurrency,
+  TokenAccount,
+  TokenCurrency,
+  Transaction,
+} from "../types";
 import type {
   PlatformAccount,
   PlatformCurrency,
   PlatformTransaction,
 } from "./types";
 
-export function accountToPlatformAccount(account: Account): PlatformAccount {
+export function accountToPlatformAccount(
+  account: Account | TokenAccount,
+  parentAccount?: Account
+): PlatformAccount {
+  if (account.type === "TokenAccount") {
+    if (!parentAccount) {
+      throw new Error("No parent account account provided for token account");
+    }
+
+    return {
+      id: account.id,
+      name: `${parentAccount.name} (${account.token.ticker})`,
+      address: parentAccount.freshAddress,
+      currency: account.token.id,
+      balance: account.balance,
+      spendableBalance: account.spendableBalance,
+      blockHeight: parentAccount.blockHeight,
+      lastSyncDate: parentAccount.lastSyncDate,
+    };
+  }
+
   return {
     id: account.id,
     name: account.name,
@@ -20,8 +46,29 @@ export function accountToPlatformAccount(account: Account): PlatformAccount {
   };
 }
 export function currencyToPlatformCurrency(
-  currency: CryptoCurrency
+  currency: CryptoCurrency | TokenCurrency
 ): PlatformCurrency {
+  if (currency.type === "TokenCurrency") {
+    if (currency.parentCurrency.family !== "ethereum") {
+      throw new Error("Only ERC20 tokens are supported");
+    }
+
+    return {
+      type: "ERC20TokenCurrency",
+      id: currency.id,
+      ticker: currency.ticker,
+      contract: currency.contractAddress,
+      name: currency.name,
+      chain: currency.parentCurrency.id,
+      color: currency.parentCurrency.color,
+      units: currency.units.map((unit) => ({
+        name: unit.name,
+        code: unit.code,
+        magnitude: unit.magnitude,
+      })),
+    };
+  }
+
   return {
     type: currency.type,
     id: currency.id,
