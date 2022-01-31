cde2bffd7f4c321fa424f4686e71dc038b379605
wa-aal <93649819+wa-aal@users.noreply.github.com>
2022-01-31 13:47:35 +0100

    Cosmos JS integration (#1665)
    * increase gas amplifier
    
    * fix payload construction
    
    handle payload atomic construction
    
    * More accurate gas amplifier
    
    Co-authored-by: Alexandre Alouit <alexandre.alouit@gmail.com>

--- src/families/cosmos/js-buildTransaction.ts
+++ src/families/cosmos/js-buildTransaction.ts
@@ -21,106 +21,160 @@ const buildTransaction = async (
 
   const msg: Array<{ typeUrl: string; value: any }> = [];
 
+  // Ledger Live is able to build transaction atomically,
+  // Take care expected data are complete before push msg.
+  // Otherwise, the transaction is silently returned intact.
+
+  let isComplete = true;
+
   switch (transaction.mode) {
     case "send":
-      msg.push({
-        typeUrl: "/cosmos.bank.v1beta1.MsgSend",
-        value: {
-          fromAddress: account.freshAddress,
-          toAddress: transaction.recipient,
-          amount: [
-            {
-              denom: account.currency.units[1].code,
-              amount: transaction.amount.toString(),
-            },
-          ],
-        },
-      });
-      break;
-
-    case "delegate":
-      transaction.validators.forEach((validator) => {
+      if (!transaction.recipient || !transaction.amount) {
+        isComplete = false;
+      } else {
         msg.push({
-          typeUrl: "/cosmos.staking.v1beta1.MsgDelegate",
+          typeUrl: "/cosmos.bank.v1beta1.MsgSend",
           value: {
-            delegatorAddress: account.freshAddress,
-            validatorAddress: validator.address,
+            fromAddress: account.freshAddress,
+            toAddress: transaction.recipient,
             amount: [
               {
                 denom: account.currency.units[1].code,
-                amount: validator.amount.toString(),
+                amount: transaction.amount.toString(),
               },
             ],
           },
         });
-      });
+      }
+      break;
+
+    case "delegate":
+      if (!transaction.validators || transaction.validators.length < 1) {
+        isComplete = false;
+      } else {
+        transaction.validators.forEach((validator) => {
+          if (!validator.address || !validator.amount) {
+            isComplete = false;
+          }
+
+          msg.push({
+            typeUrl: "/cosmos.staking.v1beta1.MsgDelegate",
+            value: {
+              delegatorAddress: account.freshAddress,
+              validatorAddress: validator.address,
+              amount: [
+                {
+                  denom: account.currency.units[1].code,
+                  amount: validator.amount.toString(),
+                },
+              ],
+            },
+          });
+        });
+      }
       break;
 
     case "undelegate":
-      msg.push({
-        typeUrl: "/cosmos.staking.v1beta1.MsgUndelegate",
-        value: {
-          delegatorAddress: account.freshAddress,
-          validatorAddress: transaction.validators[0].address,
-          amount: [
-            {
-              denom: account.currency.units[1].code,
-              amount: transaction.validators[0].amount.toString(),
-            },
-          ],
-        },
-      });
+      if (
+        !transaction.validators ||
+        transaction.validators.length < 1 ||
+        !transaction.validators[0].address ||
+        !transaction.validators[0].amount
+      ) {
+        isComplete = false;
+      } else {
+        msg.push({
+          typeUrl: "/cosmos.staking.v1beta1.MsgUndelegate",
+          value: {
+            delegatorAddress: account.freshAddress,
+            validatorAddress: transaction.validators[0].address,
+            amount: [
+              {
+                denom: account.currency.units[1].code,
+                amount: transaction.validators[0].amount.toString(),
+              },
+            ],
+          },
+        });
+      }
       break;
 
     case "redelegate":
-      msg.push({
-        typeUrl: "/cosmos.staking.v1beta1.MsgBeginRedelegate",
-        value: {
-          validatorSrcAddress: transaction.cosmosSourceValidator,
-          delegatorAddress: account.freshAddress,
-          validatorAddress: transaction.validators[0].address,
-          amount: [
-            {
-              denom: account.currency.units[1].code,
-              amount: transaction.validators[0].amount.toString(),
-            },
-          ],
-        },
-      });
+      if (
+        !transaction.cosmosSourceValidator ||
+        !transaction.validators ||
+        transaction.validators.length < 1 ||
+        !transaction.validators[0].address ||
+        !transaction.validators[0].amount
+      ) {
+        isComplete = false;
+      } else {
+        msg.push({
+          typeUrl: "/cosmos.staking.v1beta1.MsgBeginRedelegate",
+          value: {
+            validatorSrcAddress: transaction.cosmosSourceValidator,
+            delegatorAddress: account.freshAddress,
+            validatorAddress: transaction.validators[0].address,
+            amount: [
+              {
+                denom: account.currency.units[1].code,
+                amount: transaction.validators[0].amount.toString(),
+              },
+            ],
+          },
+        });
+      }
       break;
 
     case "claimReward":
-      msg.push({
-        typeUrl: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
-        value: {
-          delegatorAddress: account.freshAddress,
-          validatorAddress: transaction.validators[0].address,
-        },
-      });
+      if (
+        !transaction.validators ||
+        transaction.validators.length < 1 ||
+        !transaction.validators[0].address
+      ) {
+        isComplete = false;
+      } else {
+        msg.push({
+          typeUrl: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
+          value: {
+            delegatorAddress: account.freshAddress,
+            validatorAddress: transaction.validators[0].address,
+          },
+        });
+      }
       break;
 
     case "claimRewardCompound":
-      msg.push({
-        typeUrl: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
-        value: {
-          delegatorAddress: account.freshAddress,
-          validatorAddress: transaction.validators[0].address,
-        },
-      });
+      if (
+        !transaction.validators ||
+        transaction.validators.length < 1 ||
+        !transaction.validators[0].address ||
+        !transaction.validators[0].amount
+      ) {
+        isComplete = false;
+      } else {
+        msg.push({
+          typeUrl: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
+          value: {
+            delegatorAddress: account.freshAddress,
+            validatorAddress: transaction.validators[0].address,
+          },
+        });
 
-      msg.push({
-        typeUrl: "/cosmos.staking.v1beta1.MsgDelegate",
-        value: {
-          delegatorAddress: account.freshAddress,
-          validatorAddress: transaction.validators[0].address,
-          amount: [
-            {
-              denom: account.currency.units[1].code,
-              amount: transaction.validators[0].amount.toString(),
-            },
-          ],
-        },
-      });
+        msg.push({
+          typeUrl: "/cosmos.staking.v1beta1.MsgDelegate",
+          value: {
+            delegatorAddress: account.freshAddress,
+            validatorAddress: transaction.validators[0].address,
+            amount: [
+              {
+                denom: account.currency.units[1].code,
+                amount: transaction.validators[0].amount.toString(),
+              },
+            ],
+          },
+        });
+      }
       break;
   }
 
@@ -139,6 +193,7 @@ const buildTransaction = async (
   return {
     messages: msg,
     auth: authInfoBytes,
+    isComplete,
   };
 };
 
