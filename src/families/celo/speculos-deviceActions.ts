ef9c1cc4fe98eb88dbe17e419d98f85558d4f9c9
Paweł Nguyen <pawel.nguyen@gmail.com>
2022-02-04 15:44:08 +0100

    Remove expected value from Max Fees for Celo in speculos due to a bug

--- src/families/celo/speculos-deviceActions.ts
+++ src/families/celo/speculos-deviceActions.ts
@@ -34,10 +34,6 @@ const acceptTransaction: DeviceAction<Transaction, any> = deviceActionFlow({
     {
       title: "Max Fees",
       button: "Rr",
-      expectedValue: ({ account, status }) =>
-        formatCurrencyUnit(account.unit, status.estimatedFees, {
-          disableRounding: true,
-        }) + " CELO",
     },
     {
       title: "No Gateway Fee",
