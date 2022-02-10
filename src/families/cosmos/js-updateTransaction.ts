e8ad8f3141c1076e01706f0a8590edbac5619b18
Alexandre Alouit <alexandre.alouit@gmail.com>
2022-02-10 22:02:47 +0100

    update transaction: more strict types

--- src/families/cosmos/js-updateTransaction.ts
+++ src/families/cosmos/js-updateTransaction.ts
@@ -1,11 +1,16 @@
-const updateTransaction = (t, patch) => {
+import { Transaction } from "./types";
+
+const updateTransaction = (
+  t: Transaction,
+  patch: Partial<Transaction>
+): Transaction => {
   if ("mode" in patch && patch.mode !== t.mode) {
     return { ...t, ...patch, gas: null, fees: null };
   }
 
   if (
     "validators" in patch &&
-    patch.validators.length !== t.validators.length
+    patch.validators?.length !== t.validators.length
   ) {
     return { ...t, ...patch, gas: null, fees: null };
   }
