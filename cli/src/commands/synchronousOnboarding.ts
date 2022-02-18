1aaa42dda68cbf513591a64dd1aafceab608baef
Thomas Laforge <thomas.laforge@ledger.fr>
2022-02-18 11:09:10 +0100

    splitting and keep independant

--- cli/src/commands/synchronousOnboarding.ts
+++ cli/src/commands/synchronousOnboarding.ts
@@ -1,14 +1,60 @@
 import { from, interval, Observable } from "rxjs";
 import { withDevice } from "@ledgerhq/live-common/lib/hw/deviceAccess";
-import getDeviceInfo from "@ledgerhq/live-common/lib/hw/getDeviceInfo";
-import { deviceOpt } from "../scan";
-import { concatMap, finalize, take } from "rxjs/operators";
+import getOnboardingStatus from "@ledgerhq/live-common/lib/hw/getOnboardingStatus";
+import { concatMap, tap } from "rxjs/operators";
+
 export default {
+  description:
+    "track the onboarding status of your nano",
+  args: [
+    {
+      name: "refresh",
+      alias: "r",
+      desc: "refresh rate in milliseconds",
+    },
+  ],
   job: ({
     device,
   }: Partial<{
     device: string;
   }>) => interval(1000).pipe(
-      concatMap(() => withDevice(device || "")((t) => from(getDeviceInfo(t)))),
+      concatMap(() => withDevice(device || "")((t) => from(getOnboardingStatus(t)))),
+      // pairwise()
+      tap((onboarding => {
+        if(!onboarding){
+          return
+        }
+
+        let sentence;
+        if(onboarding.isOnboarded){
+          sentence = "ONBOARDED";
+        }
+        else {
+          if(onboarding.isRecoveryMode){
+            sentence = "IN RECOVERY MODE";
+          }
+          else {
+            if(onboarding.isSeedRecovery){
+              sentence = "SEED RECOVERY";
+            }
+            // New seed
+            else {
+              sentence = "NEW SEED";
+              if(onboarding.isConfirming){
+                sentence += " - CONFIRMING";
+              }
+              // writing
+              else {
+                sentence += " - WRITING";
+              }
+            }
+            sentence += ` : ${onboarding.currentWord} / ${onboarding.seedSize}`;
+          }
+        }
+
+        console.log(`---------------------
+    ${sentence}
+---------------------`);
+      }))
     )
 };
---
+++ src/types/onboarding.ts
@@ -0,0 +1,14 @@
+export type SeedSize = 12 | 18 | 24;
+
+export type OnboardingInfo = {
+  isOnboarded: boolean;
+  // in recovery mode vs in normal mode
+  isRecoveryMode?: boolean;
+  // seed Recovery vs new seed
+  isSeedRecovery?: boolean;
+  // confirming vs writing
+  isConfirming?: boolean;
+  seedSize?: SeedSize;
+  // Starting from 1 to totalNbSeedWords
+  currentWord?: number;
+}
