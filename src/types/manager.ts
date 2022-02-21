08391bf35a13643a0578c4d4446dde9bc572ec7e
Thomas Laforge <thomas.laforge@ledger.fr>
2022-02-21 13:57:13 +0100

    remove duplicate

--- src/types/manager.ts
+++ src/types/manager.ts
@@ -11,19 +11,6 @@ export type LedgerScriptParams = {
   hash: string;
   perso: string;
 };
-export type SeedSize = 12 | 18 | 24;
-export type OnboardingInfo = {
-  isOnboarded: boolean;
-  // in recovery mode vs in normal mode
-  isRecoveryMode?: boolean;
-  // seed Recovery vs new seed
-  isSeedRecovery?: boolean;
-  // confirming vs writing
-  isConfirming?: boolean;
-  seedSize?: SeedSize;
-  // Starting from 1 to totalNbSeedWords
-  currentWord?: number;
-};
 
 export type DeviceInfo = {
   mcuVersion: string;
