29e7709ef87b38912c64c915c07a1689c001d1f4
Pavan Vora <pavanvora.cs@gmail.com>
2022-02-23 16:09:08 +0530

    receive flow verify address fix

--- src/families/cardano/hw-getAddress.ts
+++ src/families/cardano/hw-getAddress.ts
@@ -9,19 +9,20 @@ import { StakeChain } from "./types";
 import { CARDANO_NETWORK_ID, STAKING_ADDRESS_INDEX } from "./constants";
 import { utils as TyphonUtils } from "@stricahq/typhonjs";
 
-const resolver: Resolver = async (transport, { path }) => {
+const resolver: Resolver = async (transport, { path, verify }) => {
   const spendingPath = getBipPathFromString(path);
   const stakingPathString = getBipPathString({
     account: spendingPath.account,
     chain: StakeChain.stake,
     index: STAKING_ADDRESS_INDEX,
   });
+  const network =
+    CARDANO_NETWORK_ID === Networks.Mainnet.networkId
+      ? Networks.Mainnet
+      : Networks.Testnet;
   const ada = new Ada(transport);
   const r = await ada.deriveAddress({
-    network:
-      CARDANO_NETWORK_ID === Networks.Mainnet.networkId
-        ? Networks.Mainnet
-        : Networks.Testnet,
+    network,
     address: {
       type: AddressType.BASE_PAYMENT_KEY_STAKE_KEY,
       params: {
@@ -30,6 +31,18 @@ const resolver: Resolver = async (transport, { path }) => {
       },
     },
   });
+  if (verify) {
+    await ada.showAddress({
+      network,
+      address: {
+        type: AddressType.BASE_PAYMENT_KEY_STAKE_KEY,
+        params: {
+          spendingPath: str_to_path(path),
+          stakingPath: str_to_path(stakingPathString),
+        },
+      },
+    });
+  }
   return {
     address: TyphonUtils.getAddressFromHex(r.addressHex).getBech32(),
     publicKey: "",
