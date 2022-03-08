cb27b77f73ec0535bd4f77c1c044078c0967d2ae
lambertkevin <kevin.lambert@ledger.fr>
2022-03-08 11:57:29 +0100

    Update NFT Id to contain currency

--- src/nft/nftId.ts
+++ src/nft/nftId.ts
@@ -1,17 +1,19 @@
 export const encodeNftId = (
   accountId: string,
   contract: string,
-  tokenId: string
+  tokenId: string,
+  currencyId: string
 ): string => {
-  return `${accountId}+${contract}+${tokenId.toString()}`;
+  return `${accountId}+${contract}+${tokenId}+${currencyId}`;
 };
 
 export const decodeNftId = (id: string): unknown => {
-  const [accountId, contract, tokenId] = id.split("+");
+  const [accountId, contract, tokenId, currencyId] = id.split("+");
 
   return {
     accountId,
     contract,
     tokenId,
+    currencyId,
   };
 };
