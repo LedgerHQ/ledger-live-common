cef36c151a99cb4a423f30b04d0b2b4bf181450d
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2022-03-14 18:32:06 +0100

    increase maxAccount to have more coverage

--- src/families/cosmos/specs.ts
+++ src/families/cosmos/specs.ts
@@ -25,7 +25,7 @@ import {
 import { DeviceModelId } from "@ledgerhq/devices";
 
 const minAmount = new BigNumber(20000);
-const maxAccounts = 24;
+const maxAccounts = 32;
 
 // amounts of delegation are not exact so we are applying an approximation
 function approximateValue(value) {
