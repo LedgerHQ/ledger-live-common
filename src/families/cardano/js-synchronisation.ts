e5445bab1c37e9b3e778717ba0b4da000e61c5ea
Pavan Vora <pavanvora.cs@gmail.com>
2022-03-22 10:45:20 +0530

    remove tokens support code

--- src/families/cardano/js-synchronisation.ts
+++ src/families/cardano/js-synchronisation.ts
@@ -1,17 +1,11 @@
-import type {
-  Account,
-  CryptoCurrency,
-  Operation,
-  SubAccount,
-  TokenAccount,
-} from "../../types";
+import type { Operation } from "../../types";
 import {
   GetAccountShape,
   makeScanAccounts,
   mergeOps,
 } from "../../bridge/jsHelpers";
 import { makeSync } from "../../bridge/jsHelpers";
-import { emptyHistoryCache, encodeAccountId } from "../../account";
+import { encodeAccountId } from "../../account";
 
 import BigNumber from "bignumber.js";
 import Ada from "@cardano-foundation/ledgerjs-hw-app-cardano";
@@ -27,7 +21,6 @@ import {
   getOperationType,
   getTokenAssetId,
   getTokenDiff,
-  // mergeTokens,
 } from "./logic";
 import { encodeOperationId } from "../../operation";
 import { getOperations } from "./api/getOperations";
@@ -180,91 +173,6 @@ function filterUtxos(
   return utxos;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function prepareTokensAccounts({
-  initialAccount,
-  parentAccountId,
-  tokensBalance,
-  operationsMap,
-  parentCurrency,
-}: {
-  parentCurrency: CryptoCurrency;
-  parentAccountId: string;
-  initialAccount: Account | undefined;
-  tokensBalance: Array<Token>;
-  operationsMap: Record<string, Array<Operation>>;
-}): Array<SubAccount> {
-  function getNewSubAccount(
-    token: Token,
-    operations: Array<Operation>
-  ): TokenAccount {
-    const assetId = getTokenAssetId(token);
-    return {
-      type: "TokenAccount",
-      id: assetId,
-      parentId: parentAccountId,
-      token: {
-        type: "TokenCurrency",
-        name: token.assetName,
-        // TODO: get ticker from registry
-        ticker: "",
-        //TODO: get units from registry
-        units: [
-          {
-            name: token.assetName,
-            code: token.assetName,
-            magnitude: 0,
-            showAllDigits: true,
-          },
-        ],
-        id: assetId,
-        // using assetId as cardano do not have contractAddress
-        contractAddress: assetId,
-        parentCurrency,
-        tokenType: "", //TODO: give tokenType
-      },
-      balance: token.amount,
-      spendableBalance: token.amount,
-      creationDate: new Date(),
-      operationsCount: operations.length,
-      operations,
-      pendingOperations: [],
-      starred: false,
-      balanceHistoryCache: emptyHistoryCache,
-      swapHistory: [],
-    };
-  }
-
-  const subAccountsMap = (initialAccount?.subAccounts || []).reduce(
-    (finalMap, subAccount) => {
-      finalMap[subAccount.id] = subAccount;
-      return finalMap;
-    },
-    {} as Record<string, SubAccount>
-  );
-
-  tokensBalance.forEach((t) => {
-    const tokenAccountId = getTokenAssetId(t);
-    if (subAccountsMap[tokenAccountId]) {
-      subAccountsMap[tokenAccountId].balance = t.amount;
-    } else {
-      subAccountsMap[tokenAccountId] = getNewSubAccount(t, []);
-    }
-  });
-
-  for (const tokenAccountId in operationsMap) {
-    if (subAccountsMap[tokenAccountId]) {
-      const oldOperations = subAccountsMap[tokenAccountId].operations;
-      const newOperations = operationsMap[tokenAccountId];
-
-      const operations = mergeOps(oldOperations, newOperations);
-      subAccountsMap[tokenAccountId].operations = operations;
-    }
-  }
-
-  return Object.values(subAccountsMap);
-}
-
 export const getAccountShape: GetAccountShape = async (info) => {
   const {
     transport,
@@ -343,7 +251,6 @@ export const getAccountShape: GetAccountShape = async (info) => {
     (total, u) => total.plus(u.amount),
     new BigNumber(0)
   );
-  // const tokensBalance = mergeTokens(utxos.map((u) => u.tokens).flat());
 
   const allNewOperations = newTransactions
     .map((t) => mapApiTxToOperation(t, accountId, accountCredentialsMap))
@@ -355,14 +262,6 @@ export const getAccountShape: GetAccountShape = async (info) => {
     Object.values(stableOperationsIds),
     accountNewOperations
   );
-
-  // const subAccounts = prepareTokensAccounts({
-  //   parentAccountId: accountId,
-  //   parentCurrency: currency,
-  //   initialAccount,
-  //   tokensBalance,
-  //   operationsMap: newOperations,
-  // });
 
   const stakeCredential = getAccountStakeCredential(xpub, accountIndex);
   // TODO: remove fix currencyId cardano_testnet
@@ -386,7 +285,6 @@ export const getAccountShape: GetAccountShape = async (info) => {
     balance: accountBalance,
     spendableBalance: accountBalance,
     operations: accountOperations,
-    // subAccounts,
     freshAddresses,
     freshAddress: freshAddresses[0].address,
     freshAddressPath: freshAddresses[0].derivationPath,
