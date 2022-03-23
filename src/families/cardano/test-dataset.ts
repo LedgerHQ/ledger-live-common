d23fb51ee43f1e2d73714fc063f70ff9ffc82a22
Pavan Vora <pavanvora.cs@gmail.com>
2022-03-23 11:41:58 +0530

    update cardano dataset test

--- src/families/cardano/test-dataset.ts
+++ src/families/cardano/test-dataset.ts
@@ -1,6 +1,9 @@
+import BigNumber from "bignumber.js";
 import type { DatasetTest } from "../../types";
 import { cardanoRawAccount1 } from "./datasets/rawAccount.1";
 import { cardanoScanAccount1 } from "./datasets/scanAccount.1";
+import { CardanoMinAmountError } from "./errors";
+import { fromTransactionRaw } from "./transaction";
 import type { Transaction } from "./types";
 
 const dataset: DatasetTest<Transaction> = {
@@ -12,8 +15,23 @@ const dataset: DatasetTest<Transaction> = {
         {
           raw: cardanoRawAccount1,
           transactions: [
-            // Invalid address test
-            // Minimum UTXO value test
+            {
+              //TODO: verify this test
+              name: "amount less then minimum",
+              transaction: fromTransactionRaw({
+                family: "cardano",
+                recipient:
+                  "addr_test1qpl90kc2jl5kr9tev0s7vays9yhwcdnq8nlylyk4dqsdq3g466elxnxwrzwq72pvp5akenj30t5s9et7frfvrxxx8xcsxrzs87",
+                amount: "0.1",
+                mode: "send",
+              }),
+              expectedStatus: {
+                amount: new BigNumber("0.1"),
+                errors: {
+                  amount: new CardanoMinAmountError(),
+                },
+              },
+            },
           ],
         },
       ],
