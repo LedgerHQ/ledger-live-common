e2f1e0991d125b74a88b8f96c1d0c24cbb77007c
Gabriel R. Soares <gabriel.soares@ledger.fr>
2022-03-31 17:06:03 +0200

    Fix linting

--- src/featureFlags/types.ts
+++ src/featureFlags/types.ts
@@ -1,5 +1,9 @@
-export type FeatureId = "learn" | "pushNotifications" | "llmUsbFirmwareUpdate" | string;
 // Add others with union (e.g. "learn" | "market" | "foo")
+export type FeatureId =
+  | "learn"
+  | "pushNotifications"
+  | "llmUsbFirmwareUpdate"
+  | string;
 
 // We use objects instead of direct booleans for potential future improvements
 // like feature versioning etc
