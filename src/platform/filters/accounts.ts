733b73c2c33b57afeeabdb3f0a0763dfc4d4c236
Hugo Morosini <hugo.morosini@gmail.com>
2022-04-12 19:10:45 +0200

    added account filtering logic

---
+++ src/platform/filters/accounts.ts
@@ -0,0 +1,25 @@
+import { PlatformAccount } from "../types";
+import minimatch from "minimatch";
+
+export type AccountFilters = {
+  currencies?: string[];
+};
+
+export function filterAccount(
+  accounts: PlatformAccount[],
+  filters: AccountFilters
+): PlatformAccount[] {
+  return accounts.filter((account) => {
+    if (filters.currencies) {
+      const regexes = filters.currencies.map((filter) =>
+        minimatch.makeRe(filter)
+      );
+
+      if (regexes.every((regex) => !account.currency.match(regex))) {
+        return false;
+      }
+    }
+
+    return true;
+  });
+}
