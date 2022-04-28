3c5650d6d5dfacade39c46676a7eba3e1aff0745
konoart <konoart@solana.com>
2022-04-28 15:14:15 +0200

    solana set blockhash cache time to 15 secs

--- src/families/solana/api/cached.ts
+++ src/families/solana/api/cached.ts
@@ -98,7 +98,7 @@ export function cached(api: ChainAPI): ChainAPI {
     getLatestBlockhash: makeLRUCache(
       api.getLatestBlockhash,
       cacheKeyEmpty,
-      seconds(30)
+      seconds(15)
     ),
 
     getFeeForMessage: makeLRUCache(
