ad6dd000bcf036b957bba118d66c4db5b33aabf5
Pavan Vora <pavanvora.cs@gmail.com>
2022-05-09 18:37:38 +0530

    getTransactionStatus method update

--- src/families/cardano/js-getTransactionStatus.ts
+++ src/families/cardano/js-getTransactionStatus.ts
@@ -6,62 +6,72 @@ import {
   InvalidAddress,
   AmountRequired,
 } from "@ledgerhq/errors";
-import type { Account, SubAccount, TransactionStatus } from "../../types";
-import type { CardanoResources, Transaction } from "./types";
+import type { Account, TransactionStatus } from "../../types";
+import type { CardanoResources, Token, Transaction } from "./types";
 import { isValidAddress } from "./logic";
 import { utils as TyphonUtils } from "@stricahq/typhonjs";
 import { CardanoMinAmountError } from "./errors";
 import { AccountAwaitingSendPendingOperations } from "../../errors";
 import { getNetworkParameters } from "./networks";
+import { decodeTokenAssetId, decodeTokenCurrencyId } from "./buildSubAccounts";
+import estimateMaxSpendable from "./js-estimateMaxSpendable";
 
-const getTokenTransactionStatus = async (
+const getTransactionStatus = async (
   a: Account,
   t: Transaction
 ): Promise<TransactionStatus> => {
   const errors: Record<string, Error> = {};
   const warnings: Record<string, Error> = {};
   const useAllAmount = !!t.useAllAmount;
-
-  const estimatedFees = t.fees || new BigNumber(0);
-  const subAccount = (a.subAccounts || []).find((a) => {
-    return a.id === t.subAccountId;
-  }) as SubAccount;
-
-  const amount = useAllAmount ? subAccount.balance : t.amount;
-
-  if (!t.fees) {
-    errors.fees = new FeeNotLoaded();
-  }
-
-  if (!amount.gt(0)) {
-    errors.amount = useAllAmount
-      ? new NotEnoughBalance()
-      : new AmountRequired();
-  }
-
-  return Promise.resolve({
-    errors,
-    warnings,
-    estimatedFees,
-    amount,
-    totalSpent: amount,
-  });
-};
-
-const getAccountTransactionStatus = async (
-  a: Account,
-  t: Transaction
-): Promise<TransactionStatus> => {
-  const errors: Record<string, Error> = {};
-  const warnings: Record<string, Error> = {};
-  const useAllAmount = !!t.useAllAmount;
-
-  const estimatedFees = t.fees || new BigNumber(0);
-  const amount = t.amount;
-  const totalSpent = new BigNumber(amount).plus(estimatedFees);
 
   const cardanoResources = a.cardanoResources as CardanoResources;
   const networkParams = getNetworkParameters(a.currency.id);
+
+  if (a.pendingOperations.length > 0) {
+    throw new AccountAwaitingSendPendingOperations();
+  }
+
+  const estimatedFees = t.fees || new BigNumber(0);
+  let amount = t.amount;
+  let totalSpent = estimatedFees;
+
+  const tokenAccount =
+    t.subAccountId && a.subAccounts
+      ? a.subAccounts.find((a) => {
+          return a.id === t.subAccountId;
+        })
+      : undefined;
+
+  let tokensToSend: Array<Token> = [];
+  if (t.subAccountId) {
+    // Token transaction
+    if (!tokenAccount || tokenAccount.type !== "TokenAccount") {
+      throw new Error("TokenAccount not found");
+    }
+    const { assetId } = decodeTokenCurrencyId(tokenAccount.token.id);
+    const { policyId, assetName } = decodeTokenAssetId(assetId);
+    amount = t.useAllAmount ? tokenAccount.balance : t.amount;
+    totalSpent = amount;
+    tokensToSend = [
+      {
+        policyId,
+        assetName,
+        amount,
+      },
+    ];
+  } else {
+    // ADA transaction
+    amount = t.useAllAmount
+      ? await estimateMaxSpendable({ account: a, transaction: t })
+      : amount;
+    totalSpent = amount.plus(estimatedFees);
+  }
+
+  const minTransactionAmount = TyphonUtils.calculateMinUtxoAmount(
+    tokensToSend,
+    new BigNumber(cardanoResources.protocolParams.lovelacePerUtxoWord),
+    false
+  );
 
   if (!t.fees) {
     errors.fees = new FeeNotLoaded();
@@ -73,21 +83,18 @@ const getAccountTransactionStatus = async (
     errors.recipient = new InvalidAddress();
   }
 
-  const minTransactionAmount = TyphonUtils.calculateMinUtxoAmount(
-    [],
-    new BigNumber(cardanoResources.protocolParams.lovelacePerUtxoWord),
-    false
-  );
-
   if (!amount.gt(0)) {
     errors.amount = useAllAmount
       ? new NotEnoughBalance()
       : new AmountRequired();
-  } else if (amount.lt(minTransactionAmount)) {
+  } else if (!t.subAccountId && amount.lt(minTransactionAmount)) {
     errors.amount = new CardanoMinAmountError("", {
       amount: minTransactionAmount.div(1e6).toString(),
     });
-  } else if (totalSpent.gt(a.balance)) {
+  } else if (
+    (tokenAccount && totalSpent.gt(tokenAccount.balance)) ||
+    totalSpent.gt(a.balance)
+  ) {
     errors.amount = new NotEnoughBalance();
   }
 
@@ -98,19 +105,6 @@ const getAccountTransactionStatus = async (
     amount,
     totalSpent,
   });
-};
-
-const getTransactionStatus = (
-  a: Account,
-  t: Transaction
-): Promise<TransactionStatus> => {
-  if (a.pendingOperations.length > 0) {
-    throw new AccountAwaitingSendPendingOperations();
-  }
-
-  return t.subAccountId
-    ? getTokenTransactionStatus(a, t)
-    : getAccountTransactionStatus(a, t);
 };
 
 export default getTransactionStatus;
